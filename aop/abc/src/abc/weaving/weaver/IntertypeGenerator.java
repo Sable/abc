ad6db30f6cb0e11346e47b4f35c37a4b08fb4893
olhotak <olhotak@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-04-28 13:11:09 +0000

    Forgot to cva add this file

---
+++ aop/abc/src/abc/weaving/weaver/IntertypeGenerator.java
@@ -0,0 +1,17 @@
+package abc.weaving.weaver;
+import soot.*;
+import soot.util.*;
+import soot.jimple.*;
+import java.util.*;
+import abc.weaving.aspectinfo.*;
+import abc.weaving.matching.*;
+
+public class IntertypeGenerator {
+    public void addMethod( IntertypeMethodDecl imd ) {
+    }
+
+    public void addField( IntertypeFieldDecl ifd ) {
+        // TODO: Add dispatch methods
+    }
+
+}
