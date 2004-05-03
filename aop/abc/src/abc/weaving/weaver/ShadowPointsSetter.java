75c4337f91c8c36c1b2e7a090baecdfd9f6cce2e
hendren <hendren@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-05-03 12:03:25 +0000

    First version of ShadowPointsSetter.

---
+++ aop/abc/src/abc/weaving/weaver/ShadowPointsSetter.java
@@ -0,0 +1,18 @@
+package abc.weaving.weaver;
+
+import soot.*;
+import soot.util.*;
+import soot.jimple.*;
+import java.util.*;
+import abc.weaving.aspectinfo.*;
+import abc.weaving.matching.*;
+
+/** 
+ * @author Laurie Hendren
+ * @date May 3, 2004
+ */
+
+public class ShadowPointsSetter {
+
+    public void setShadowPoints(SootClass sc) {}
+}
