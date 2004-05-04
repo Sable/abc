353a36b24b8af3357ecf7b00973c6b3e301be78a
hendren <hendren@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-05-04 08:46:10 +0000

    Added isEmpty and has* methods.

--- aop/abc/src/abc/weaving/matching/MethodAdviceList.java
+++ aop/abc/src/abc/weaving/matching/MethodAdviceList.java
@@ -6,6 +6,8 @@ import java.util.LinkedList;
 
 /** The list(s) of advice applying to a method
  *  @author Ganesh Sittampalam
+ *  @author Laurie Hendren 
+ *       (added "isEmpty and has* methods", May 4, 2004)
  *  @date 28-Apr-04
  */
 public class MethodAdviceList {
@@ -18,6 +20,28 @@ public class MethodAdviceList {
 
     /** initialization or pre-initialization joinpoints, trigger inlining of this() calls */
     public List/*<AdviceApplication>*/ constructorAdvice=new LinkedList();
+
+    /** returns true if there is no advice */
+    public boolean isEmpty() { 
+        return(bodyAdvice.isEmpty() && 
+	       stmtAdvice.isEmpty() &&
+	       constructorAdvice.isEmpty());
+    }
+
+    /** returns true if there is any body advice */
+    public boolean hasBodyAdvice() { 
+      return !bodyAdvice.isEmpty();
+    }
+
+    /** returns true if there is any stmt advice */
+    public boolean hasStmtAdvice() {
+      return !stmtAdvice.isEmpty();
+    }
+
+    /** returns true if there is any constructor advice */
+    public boolean hasConstructorAdvice() {
+      return !constructorAdvice.isEmpty();
+    }
 
     public String toString() {
 	return "body advice: "+bodyAdvice+"\n"
