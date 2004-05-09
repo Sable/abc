a2fd67a258f5c821f1084061ce4d463570a45a85
hendren <hendren@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-05-09 17:18:33 +0000

    put in into ConstructorMain.java

--- aop/abc/tests/people/laurie/AjcNestedConstructorBug/ConstructorMain.java
+++ aop/abc/tests/people/laurie/AjcNestedConstructorBug/ConstructorMain.java
@@ -1,4 +1,14 @@
 public class ConstructorMain {
+ // Note that in the case of this() calls in a constructor, the
+ // treatment of preinitialization and initialization join points are
+ // not correct, and are sensitive to the order in which constructors
+  // are given in the class file.   
+  //
+  // Below we see everything is ok for preinit and init join points for
+  // constructors in class B,  but are not correct for class C.  The only
+  // difference is in the order in which the constructors are given.  The
+  // inlining strategy for handling this() calls must be broken in ajc.
+  
   public static void main(String args[])
     { int k = 100; 
       // These are ok, note order constructors given in class
@@ -54,5 +64,50 @@ class C extends A {
 
   C (int x, int y) { super(x+y); this.y = x+y; }
 
+}
+
+aspect ConstructorAspects {
+
+  static private int aspectnesting = 0;
+
+  static void message(String s)
+    { for (int i=0; i<aspectnesting; i++) System.out.print("---+");
+      System.out.println(s);
+    }
+
+
+  // call of all constructors 
+  pointcut allconstrcalls() :  call(*..new(..)) &&
+           !within(ConstructorAspects) && !call(java.lang..new(..));
+
+  // execution of all constructors
+  pointcut allconstrexecutions() : execution(*..new(..)) && 
+           !within(ConstructorAspects);
+
+  // intialization of all constructors
+  pointcut allconstrinitializations() : initialization(*..new(..)) &&
+           !within(ConstructorAspects);
+
+  // preinitialization of all constructors
+  pointcut allconstrpreinitializations() : preinitialization(*..new(..)) &&
+          !within(ConstructorAspects);
+
+  // before advice
+  before () : !within(ConstructorAspects) {
+	      message(
+		  "BEFORE: " +  thisJoinPointStaticPart.getSourceLocation() + 
+		  " " +thisJoinPointStaticPart.toLongString()); 
+	      aspectnesting++;
+	      }
+
+  // after advice
+  after () returning : !within(ConstructorAspects) {
+              aspectnesting--;
+	      message(
+		  "AFTER: " +  thisJoinPointStaticPart.getSourceLocation() + 
+		  " " +thisJoinPointStaticPart.toLongString()); 
+	      }
 
 }
+
+
