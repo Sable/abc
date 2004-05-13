ad264b6f48fe731fc302ed4b036d92b83477a812
ganesh <ganesh@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-05-13 16:49:12 +0000

    added missing visitor pattern stuff

--- aop/abc/src/abc/aspectj/ast/FieldPattern_c.java
+++ aop/abc/src/abc/aspectj/ast/FieldPattern_c.java
@@ -29,6 +29,32 @@ public class FieldPattern_c extends Node_c
 	this.name = name;
     }
 
+    private List copyList(List xs) {
+	return new LinkedList(xs);
+    }
+    
+
+    protected FieldPattern_c reconstruct(List modifiers,TypePatternExpr type,ClassTypeDotId name) {
+	if(!CollectionUtil.equals(modifiers,this.modifiers) 
+	   || type!=this.type 
+	   || name!=this.name) {
+	    
+	    FieldPattern_c n = (FieldPattern_c) copy();
+	    n.modifiers=copyList(modifiers);
+	    n.type=type;
+	    n.name=name;
+	    return n;
+	}
+	return this;
+    }
+
+    public Node visitChildren(NodeVisitor v) {
+	List modifiers=visitList(this.modifiers,v);
+	TypePatternExpr type=(TypePatternExpr) visitChild(this.type,v);
+	ClassTypeDotId name=(ClassTypeDotId) visitChild(this.name,v);
+	return reconstruct(modifiers,type,name);
+    }
+
     public void prettyPrint(CodeWriter w, PrettyPrinter tr) {
 
        for (Iterator i = modifiers.iterator(); i.hasNext(); ) {
