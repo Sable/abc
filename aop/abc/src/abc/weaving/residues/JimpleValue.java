bd70da4e7f4663da5a0f1a8572de38dfd2cc0edb
ganesh <ganesh@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-05-24 23:05:51 +0000

    jimple value residue

---
+++ aop/abc/src/abc/weaving/residues/JimpleValue.java
@@ -0,0 +1,30 @@
+package abc.weaving.residues;
+
+import soot.Value;
+import soot.SootMethod;
+import abc.weaving.weaver.LocalGeneratorEx;
+
+
+/** A context value that comes directly from a 
+ *  jimple value already in the current method
+ *  @author Ganesh Sittampalam
+ *  @date 30-Apr-04
+ */ 
+
+public class JimpleValue extends AbstractContextValue {
+
+    private Value value;
+
+    public JimpleValue(Value value) {
+	this.value=value;
+    }
+
+    public String toString() {
+	return "jimplevalue("+value+")";
+    }
+
+    public Value getSootValue(SootMethod method,LocalGeneratorEx localgen) {
+	return value;
+    }
+
+}
