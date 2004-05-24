b8d8e00064712592105da3b22e29c7293b3447aa
ganesh <ganesh@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-05-24 14:26:15 +0000

    foo

---
+++ aop/abc/tests/people/ganesh/this/Aspect.java
@@ -0,0 +1,11 @@
+public aspect Aspect {
+
+int countpointcuts = 0;
+
+static boolean debug = true;
+
+after (Baz x) throwing () : this(x)
+   {  System.out.println("I am in a pointcut ");
+      countpointcuts++;
+   }
+}
