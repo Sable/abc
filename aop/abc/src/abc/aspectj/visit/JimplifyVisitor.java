9f1ec505184513e56167e71be8e5bb21fb5cab1c
olhotak <olhotak@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-05-27 02:05:51 +0000

    Updated JimplifyVisitor for new SourceLocator in Soot.

--- aop/abc/src/abc/aspectj/visit/JimplifyVisitor.java
+++ aop/abc/src/abc/aspectj/visit/JimplifyVisitor.java
@@ -14,15 +14,31 @@ import abc.main.AbcTimer;
 public class JimplifyVisitor extends NodeVisitor {
     private Collection classes;
     private PCStructure hierarchy;
-    private SootResolver soot_res =  SootResolver.v();
     private InitialResolver res = SootResolver.v().getInitSourceResolver();
+    private Map classToAST = new HashMap();
+    private Node currentAST = null;
 
     public JimplifyVisitor(Collection classes, PCStructure hierarchy) {
 	this.classes = classes;
 	this.hierarchy = hierarchy;
+
+        List classProviders = new LinkedList();
+        classProviders.add( new AbcClassProvider() );
+        classProviders.add( new CoffiClassProvider() );
+        SourceLocator.v().setClassProviders(classProviders);
     }
 
     public Node override(Node n) {
+	if (n instanceof SourceFile) {
+            currentAST = n;
+	}
+	if (n instanceof ClassDecl) {
+	    String cname = ((ClassDecl)n).type().fullName();
+	    cname = hierarchy.transformClassName(cname);
+
+            classToAST.put(cname, currentAST);
+        }
+        /*
 	if (n instanceof SourceFile) {
 	    res.setAst(n);
 	}
@@ -33,6 +49,7 @@ public class JimplifyVisitor extends NodeVisitor {
             Scene.v().getSootClass(cname).setApplicationClass();
 	    // System.out.println("Jimplified class: "+cname);
 	}
+        */
 	return null;
     }
 
@@ -40,8 +57,10 @@ public class JimplifyVisitor extends NodeVisitor {
         // NOTE: if you move where the resolveClassAndSupportClasses is
         //   called,  please also move the timer code with it. LJH
 	long beforetime = System.currentTimeMillis();
-	// FIXME: I think we can do better than this????
-	soot_res.resolveClassAndSupportClasses("java.lang.Object");
+        for( Iterator classNameIt = classToAST.keySet().iterator(); classNameIt.hasNext(); ) {
+            final String className = (String) classNameIt.next();
+            SootResolver.v().resolveClassAndSupportClasses(className);
+        }
 	long aftertime = System.currentTimeMillis();
 	AbcTimer.addToSootResolve(aftertime-beforetime);
     }
@@ -59,6 +78,27 @@ public class JimplifyVisitor extends NodeVisitor {
 	}
     }
 */
+    private class AbcClassProvider implements ClassProvider {
+        public ClassSource find( String className ) {
+            if( !classes.contains(className) ) return null;
+            return new AbcClassSource(className);
+        }
+    }
+
+    private class AbcClassSource extends ClassSource {
+        AbcClassSource( String className ) {
+            super(className);
+        }
+        public void resolve( SootClass sc ) {
+            Node n = (Node) classToAST.get(className);
+            if( n == null ) {
+                throw new RuntimeException( "Frontend bug: The class "+className+" was listed in the parameter classes to JimplifyVisitor, but there's no AST for it." );
+            }
+	    res.setAst(n);
+	    res.resolveFromJavaFile(sc);
+            sc.setApplicationClass();
+        }
+    }
 }
 
 
