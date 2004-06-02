595cb379b3f447f7b2c2a369668215dfe5f348c9
ganesh <ganesh@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-06-02 10:39:52 +0000

    foo

--- aop/abc/tests/people/ganesh/proceed/Aspect2.java
+++ aop/abc/tests/people/ganesh/proceed/Aspect2.java
@@ -4,7 +4,7 @@ public aspect Aspect2 {
     //    }
 
     
-    int around(Test x): call(int Test.foo(int)) && (this(x) || target(x)) {
+    int around(Test2 x): call(int Test.foo(int)) && (this(x) || target(x)) {
 	System.out.println("advice 1 before proceed");
 	int ret=proceed(new Test2(-1));
 	System.out.println("advice 1 after proceed");
