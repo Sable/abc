68cd86d879a09fc634a4b10f7667b0a2fc107e78
ganesh <ganesh@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-06-02 16:41:16 +0000

    .cvsignore stuff

--- aop/abc/tests/people/ganesh/args/Aspect.java
+++ aop/abc/tests/people/ganesh/args/Aspect.java
@@ -7,4 +7,8 @@ before(Object a,Object b) : !within(Aspect*) && foo(a) && bar(b) && args(..)
    {  
 	System.out.println("foo: "+a+b);
    }
+
+pointcut splat(int x) : args(x);
+
+before(Object x): !within(Aspect*) && splat(x) {}
 }
