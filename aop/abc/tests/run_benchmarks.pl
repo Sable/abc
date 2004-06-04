537acfd13fe2082286b9738f5870cc11b84d14fb
c03sk <c03sk@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-06-04 10:56:54 +0000

    *** empty log message ***

--- aop/abc/tests/run_benchmarks.pl
+++ aop/abc/tests/run_benchmarks.pl
@@ -2,50 +2,51 @@
 
 $argc=@ARGV;
 if ($argc==0) {
-@benchmarks=('bean', 'gregor_bean', 'gof_adapter', 'gof_bridge', 'nullptr', 'nullptrafter', 'figure',  'quicksort', 'gregor_quicksort', 'oege_quicksort', 'LoD');
+	@benchmarks=('bean', 'gregor_bean', 'gof_adapter', 'gof_bridge', 'nullptr', 'nullptrafter', 
+		'productlines', 'telecom', 'figure',  'quicksort', 'gregor_quicksort', 'oege_quicksort', 'LoD');
 } else {
-@benchmarks=@ARGV;
+	@benchmarks=@ARGV;
 }
 #my $report="";
 
 foreach $dir (@benchmarks) {
-  print "Processing $dir\n";
-  #"$dir outout comparison:\n";
-  if (chdir $dir) {
-  system("./abcit > abcc.out 2>abccerr.out");
-  $abcout=`cat abccerr.out`;
-  $abc=0;
-  if ($abcout =~ m/Breakdown of abc phases/) {
-    system("./runit > abc.out 2>abcerr.out");
-    $abcsize=length `cat abc.out`;
-    print " abc output size: $abcsize\n";
-    $abc=1;
-  } else {
-    print " abc compilation failed\n";
-  }
-  system("./ajcit > ajcc.out 2> ajccerr.out");
-  $ajcout=`cat ajccerr.out`;
-  $ajc=0;
-  if ((length $ajcout)==0) {
-    system("./runit > ajc.out 2>ajcerr.out");
-    $ajcsize=length `cat ajc.out`;
-    print " ajc output size: $ajcsize\n";
-    $ajc=1;
-  } else {
-   print " ajc compilation failed\n";
-  }
-  if ($abc==1 && $ajc==1) {
-    $diff = `diff ajc.out abc.out`;
-    if ((length $diff)>2000) {
-      $diff="diff too long";
-    }
-    print (((length $diff) == 0 )  ? " same output\n" : " $diff\n");
-  }
-  system("rm -f abcc.out abc.out ajcc.out ajc.out abccerr.out ajccerr.out ajcerr.out abcerr.out");
-  chdir "..";
- } else {
-  print " Could not find $dir\n";
- }
+	print "Processing $dir\n";
+	#"$dir outout comparison:\n";
+	if (chdir $dir) {
+		system("./abcit > abcc.out 2>abccerr.out");
+		$abcout=`cat abccerr.out`;
+		$abc=0;
+		if ($abcout =~ m/Breakdown of abc phases/) {
+			system("./runit > abc.out 2>abcerr.out");
+			$abcsize=length `cat abc.out`;
+			print " abc output size: $abcsize\n";
+			$abc=1;
+		} else {
+			print " abc compilation failed\n";
+		}
+		system("./ajcit > ajcc.out 2> ajccerr.out");
+		$ajcout=`cat ajccerr.out`;
+		$ajc=0;
+		if ((length $ajcout)==0) {
+			system("./runit > ajc.out 2>ajcerr.out");
+			$ajcsize=length `cat ajc.out`;
+			print " ajc output size: $ajcsize\n";
+			$ajc=1;
+		} else {
+			print " ajc compilation failed\n";
+		}
+		if ($abc==1 && $ajc==1) {
+			$diff = `diff ajc.out abc.out`;
+			if ((length $diff)>2000) {
+				$diff="diff too long";
+			}
+			print (((length $diff) == 0 )  ? " same output\n" : " $diff\n");
+		}
+		system("rm -f abcc.out abc.out ajcc.out ajc.out abccerr.out ajccerr.out ajcerr.out abcerr.out");
+		chdir "..";
+	} else {
+		print " Could not find $dir\n";
+	}
 }
 
 #print "Summary: \n";
