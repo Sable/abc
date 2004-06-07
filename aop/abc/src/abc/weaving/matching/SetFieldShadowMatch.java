902ca64dc5a2fc502af1607456a68d47902a081a
c03sk <c03sk@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-06-07 16:58:38 +0000

    *** empty log message ***

--- aop/abc/src/abc/weaving/matching/SetFieldShadowMatch.java
+++ aop/abc/src/abc/weaving/matching/SetFieldShadowMatch.java
@@ -68,6 +68,7 @@ public class SetFieldShadowMatch extends StmtShadowMatch {
 								"setRHSLocal");
 			AssignStmt as=Jimple.v().newAssignStmt(l,val);
 			statements.insertBefore(as, stmt);
+			stmt.setRightOp(l);
 			stmt.redirectJumpsToThisTo(as);
     	}
     }
