f718afc4a3d50d63bc8f7467b3ec887120be1375
c03sk <c03sk@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-06-14 12:22:26 +0000

    *** empty log message ***

--- aop/abc/ajc-harness/tests/run_cases.pl
+++ aop/abc/ajc-harness/tests/run_cases.pl
@@ -1,61 +1,66 @@
 #!/usr/bin/perl -w
 
+use strict;
+
 my $argc = @ARGV;
-if ($argc!=1) {
+if ($argc<1) {
   print 
-"Usage: run_cases.pl XMLFILE
+"Usage: run_cases.pl XMLFILE [dir-filter [title-filter]]
 Runs the cases listed in XMLFILE individually. 
 Outputs passed.xml and failed.xml.
 " ;
 exit(0);
 }
 
-my $file;
-my $count=0;
+my $dirfilter="";
+if ($argc>1) {
+  $dirfilter=$ARGV[1];
+}
+
+my $titlefilter="";
+if ($argc>2) {
+  $titlefilter=$ARGV[2];
+}
+
 my $failed=0;
 my $succeeded=0; #sanity
 
-system("rm -f failed.output");
 
 my $xmlprefix="<!DOCTYPE suite SYSTEM \"../tests/ajcTestSuite.dtd\"> \n <suite> \n ";
 my $xmlsuffix="</suite> \n";
+
+system("rm -f failed.output");
+
 open(FAILED, "> failed.xml") or die "cannot open failed.xml";
 open(PASSED, "> passed.xml") or die "cannot open passed.xml";
 print FAILED $xmlprefix;
 print PASSED $xmlprefix;
 
-while (<>) {
-  my $xmlpart="";
-  my $title;
-  my $dir;
-  my $inside=0;
-  if (m/ajc-test.*dir=\"([^\"]*)\".*title=\"([^\"]*)\"/) {
-    $title=$2;
-    $dir=$1;
-    $xmlpart.=$_;
-    $inside=1;
-  } elsif (m/ajc-test.*dir=\"([^\"]*)\"/) {
-    $inside=1;
-    $dir=$1;
-    $xmlpart.=$_;
-    $_=<>;
-    $xmlpart.=$_;
-    #print $_;
-    while (!m/title=\"([^\"]*)\"/) { 
-      $_=<>;
-      $xmlpart.=$_;
-      m/ajc-test/ && die;
+
+my $count=0;
+my $countinvalid=0;
+
+sub do_case {
+  my $dir=$_[0];
+  my $title=$_[1];
+  my $xmlpart=$_[2];
+
+  if ((length $dirfilter) > 0) {
+    if ($dir !~ m/$dirfilter/) {
+      #print "skipping case (dir-filter): $dir: $title\n";
+      return "";
     }
-    m/title=\"([^\"]*)\"/;
-    $title=$1;
   }
-  if ($inside==1) {
-    $count++;
-   while (!m/\/ajc-test/) { 
-      $_=<>;
-      $xmlpart.=$_;
-      m/ajc-test / && die;
-   }
+  if ((length $titlefilter) > 0) {
+    if ($title !~ m/$titlefilter/) {
+      #print "skipping case (title-filter): $dir: $title\n";
+      return "";
+    }
+  }
+
+  $count++;
+#  print "dir: $dir\ntitle: $title\nXML: $xmlpart\n";
+
    open(TMP, "> tmp.xml") || die;
    print TMP "$xmlprefix $xmlpart $xmlsuffix";
    close TMP;
@@ -83,15 +88,30 @@ while (<>) {
      system("echo $title >> cases_with_invalid_output.txt");
      $count--;
      #die "could not find FAIL or PASS\n";
+     $countinvalid++;
    }
    print "Current status: $failed failed, $succeeded passed.\n";
-#   print "XML: \n $xmlpart \n\n";
-#   $count==2 && die;
-  }  
 }
+
+open(INPUT, "< $ARGV[0]") || die "can't open input file"; 
+my $file;
+while (<INPUT>) {
+  $file .= $_;
+}
+
+$file =~ s/<ajc-test[^>]+dir=\"([^\"]*)\"[^>]+title=\"([^\"]*)\"[^>]*>.*?<\/ajc-test>/do_case($1,$2,$&)/sge;
+
 print "Tests: $count\nFailed: $failed\nPassed: $succeeded\n";
+
+if ($countinvalid > 0 ) {
+  print "Tests with invalid output: $countinvalid.\n";
+}
 
 print FAILED $xmlsuffix;
 print PASSED $xmlsuffix;
 
 $succeeded+$failed==$count || die "i can't count!\n";
+
+
+
+# s/<ajc-test[^>]+dir=\"([^\"]*)\"[^>]+title=\"([^\"]*)\"[^>]*>.*?<\/ajc-test>/do_case($1,$2,$&)/sge
