102ce7590f8aad448ce45890fd389a803fd32da2
ganesh <ganesh@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-06-21 17:12:07 +0000

    foo

---
+++ aop/abc/tests/people/ganesh/declaresoft/Test.java
@@ -0,0 +1,21 @@
+public class Test {
+   void foo() throws TestException {
+      throw new TestException();
+   }
+
+   public static void main(String[] args) throws TestException {
+      new Test().foo();
+   }
+
+}
+
+class TestException extends Exception {
+
+}
+
+aspect Aspect {
+
+   declare soft: TestException: execution(void foo());
+
+      
+}
