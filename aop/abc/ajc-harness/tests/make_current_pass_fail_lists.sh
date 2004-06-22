3be8f3343235e759e65ba9d80aa09222e0146a37
c03sk <c03sk@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-06-22 12:23:21 +0000

    *** empty log message ***

--- aop/abc/ajc-harness/tests/make_current_pass_fail_lists.sh
+++ aop/abc/ajc-harness/tests/make_current_pass_fail_lists.sh
@@ -4,4 +4,7 @@
 cp failed.xml failed_current.xml &&
 cp passed.xml passed_current.xml &&
 cp skipped.xml skipped_current.xml &&
-cp failed.output failed_current.output;
+cp failed.output failed_current.output &&
+cp failed_check.xml  failed_check_current.xml && 
+cp passed_check.xml  passed_check_current.xml;
+
