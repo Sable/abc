8e11dfd5d83d2b9ec7a51a3d5c2b6f49069d5729
ganesh <ganesh@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-06-22 13:40:54 +0000

    updates

--- aop/abc/ajc-harness/tests/update_current_pass_fail_lists.sh
+++ aop/abc/ajc-harness/tests/update_current_pass_fail_lists.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
+FILES=full_current.output failed_current.output \
+      failed_current.xml passed_current.xml \
+      skipped_current.xml \
+      failed_check_current.xml \
+      passed_check_current.xml
+
 cd ../../ &&
-CLASSPATH= ant clean > /dev/null &&
-CLASSPATH= ant >/dev/null &&
+CLASSPATH= ant clean &&
+CLASSPATH= ant &&
 cd ajc-harness/tests &&
 nice -n 5 ./run_cases.pl ajcTests.xml >full.output 2>&1 &&
+cvs update $FILES
 ./calc_updates.pl | mail -s"test run results" abc@comlab.ox.ac.uk &&
 cp failed.xml failed_current.xml &&
 cp passed.xml passed_current.xml &&
@@ -13,4 +20,4 @@ cp full.output full_current.output &&
 cp failed_check.xml  failed_check_current.xml && 
 cp passed_check.xml  passed_check_current.xml && 
 cp skipped.xml skipped_current.xml &&
-cvs commit -m"automatic update" full_current.output failed_current.output failed_current.xml passed_current.xml >/dev/null
+cvs commit -m"automatic update" $FILES
