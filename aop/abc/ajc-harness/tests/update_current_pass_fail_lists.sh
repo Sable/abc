c23218a62833b3820e06c9cb71c729978a4d2e86
c03sk <c03sk@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-06-22 12:56:54 +0000

    *** empty log message ***

--- aop/abc/ajc-harness/tests/update_current_pass_fail_lists.sh
+++ aop/abc/ajc-harness/tests/update_current_pass_fail_lists.sh
@@ -10,4 +10,7 @@ cp failed.xml failed_current.xml &&
 cp passed.xml passed_current.xml &&
 cp failed.output failed_current.output &&
 cp full.output full_current.output &&
+cp failed_check.xml  failed_check_current.xml && 
+cp passed_check.xml  passed_check_current.xml && 
+cp skipped.xml skipped_current.xml &&
 cvs commit -m"automatic update" full_current.output failed_current.output failed_current.xml passed_current.xml >/dev/null
