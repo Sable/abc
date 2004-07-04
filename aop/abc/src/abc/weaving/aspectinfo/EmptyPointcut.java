f287cf3550360743575379c180be6301df4ad408
ganesh <ganesh@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-07-04 17:03:53 +0000

    empty pointcuts

---
+++ aop/abc/src/abc/weaving/aspectinfo/EmptyPointcut.java
@@ -0,0 +1,28 @@
+package abc.weaving.aspectinfo;
+
+import polyglot.util.Position;
+
+import soot.*;
+
+import abc.weaving.residues.*;
+
+/** A pointcut designator representing no join points
+ *  at all
+ *  @author Ganesh Sittampalam
+ *  @date 30-Apr-04
+ */
+
+public class EmptyPointcut extends LexicalPointcut{
+
+    public EmptyPointcut(Position pos) {
+	super(pos);
+    }
+
+    protected Residue matchesAt(SootClass cls,SootMethod method) {
+	return NeverMatch.v;
+    }
+    
+    public String toString() {
+	return "empty";
+    }
+}
