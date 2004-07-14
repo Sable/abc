292ed6ec9186444c06e46839a189d70d25c118ea
ganesh <ganesh@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-07-14 21:04:11 +0000

    fixes

--- aop/abc/ajc-harness/tests/update_current_pass_fail_lists.sh
+++ aop/abc/ajc-harness/tests/update_current_pass_fail_lists.sh
@@ -1,20 +1,20 @@
-#!/bin/sh
+#!/bin/sh -e
 
 FILES="full_current.output failed_current.output \
       failed_current.xml passed_current.xml \
       skipped_current.xml"
 
-cd ../../ &&
-CLASSPATH= ant clobber &&
-CLASSPATH= ant &&
-cd ajc-harness/tests &&
-nice -n 5 ./runtests abcTests.xml >/dev/null &&
+cd ../../
+CLASSPATH= ant clobber
+CLASSPATH= ant
+cd ajc-harness/tests
+nice -n 5 ./runtests abcTests.xml
 cvs update $FILES
 perl -pe's/\&/\&amp;/g' -i passed.xml failed.xml skipped.xml
-./calc_updates.pl | mail -s"test run results" abc@comlab.ox.ac.uk &&
-cp failed.xml failed_current.xml &&
-cp passed.xml passed_current.xml &&
-cp skipped.xml skipped_current.xml &&
-cp failed.output failed_current.output &&
-cp full.output full_current.output &&
+./calc_updates.pl | mail -s"test run results" abc@comlab.ox.ac.uk
+cp failed.xml failed_current.xml
+cp passed.xml passed_current.xml
+cp skipped.xml skipped_current.xml
+cp failed.output failed_current.output
+cp full.output full_current.output
 cvs commit -m"automatic update" $FILES
