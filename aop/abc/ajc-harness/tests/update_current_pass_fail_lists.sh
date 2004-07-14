c3f65a18fb94d394a1e20cf2050f469615b70619
ganesh <ganesh@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-07-14 20:11:39 +0000

    switch to new test harness

--- aop/abc/ajc-harness/tests/update_current_pass_fail_lists.sh
+++ aop/abc/ajc-harness/tests/update_current_pass_fail_lists.sh
@@ -2,22 +2,19 @@
 
 FILES="full_current.output failed_current.output \
       failed_current.xml passed_current.xml \
-      skipped_current.xml \
-      failed_check_current.xml \
-      passed_check_current.xml"
+      skipped_current.xml"
 
 cd ../../ &&
 CLASSPATH= ant clobber &&
 CLASSPATH= ant &&
 cd ajc-harness/tests &&
-nice -n 5 ./run_cases_abc.pl abcTests.xml >full.output 2>&1 &&
+nice -n 5 ./runtests abcTests.xml >/dev/null &&
 cvs update $FILES
+perl -pe's/\&/\&amp;/g' -i passed.xml failed.xml skipped.xml
 ./calc_updates.pl | mail -s"test run results" abc@comlab.ox.ac.uk &&
 cp failed.xml failed_current.xml &&
 cp passed.xml passed_current.xml &&
+cp skipped.xml skipped_current.xml &&
 cp failed.output failed_current.output &&
 cp full.output full_current.output &&
-cp failed_check.xml  failed_check_current.xml && 
-cp passed_check.xml  passed_check_current.xml && 
-cp skipped.xml skipped_current.xml &&
 cvs commit -m"automatic update" $FILES
