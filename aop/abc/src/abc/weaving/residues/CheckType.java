5f00a9c3504073fadbba79a1b9dceb90a500db12
ganesh <ganesh@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-07-22 11:51:00 +0000

    an always-null return value from a joinpoint should not match any type but Object

--- aop/abc/src/abc/weaving/residues/CheckType.java
+++ aop/abc/src/abc/weaving/residues/CheckType.java
@@ -60,6 +60,8 @@ public class CheckType extends Residue {
 	} else {
 	    FastHierarchy hier=Scene.v().getOrMakeFastHierarchy();
 
+	    if(from instanceof NullType) return NeverMatch.v;
+
 	    if(hier.canStoreType(from,to)) 
 	    	return AlwaysMatch.v;
 	    // For strict ajc compliance, we *must* eliminate this much, and
