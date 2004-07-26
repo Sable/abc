5a9dc9b6e33efe21cf3012849b953e5b0f9312fe
c03sk <c03sk@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-07-26 13:27:11 +0000

    *** empty log message ***

---
+++ aop/abc/tests/people/sascha/innerAnonymous/Plain.java
@@ -0,0 +1,24 @@
+
+aspect Plain0 {
+	declare precedence : Plain4, Plain3, Plain0;
+	int around() :  call(int Test.* (..)) //adviceexecution() && within(Plain2)
+	{
+		class LocalClass2 
+		{
+			int run() 
+			{
+				System.out.println("42");
+				return proceed();
+			}
+		}
+		
+		Runnable lc=new Runnable() {
+			public void run() 
+			{
+				new LocalClass2().run();
+			}
+		};
+		lc.run();
+		return 0;
+	}
+}
