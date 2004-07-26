c9b9f5689e417105725fde6d0b6edd5b1fd72e5d
c03sk <c03sk@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-07-26 13:29:03 +0000

    *** empty log message ***

--- aop/abc/tests/people/sascha/innerAnonymous/Plain.java
+++ aop/abc/tests/people/sascha/innerAnonymous/Plain.java
@@ -1,6 +1,6 @@
 
 aspect Plain0 {
-	declare precedence : Plain4, Plain3, Plain0;
+	//declare precedence : Plain4, Plain3, Plain0;
 	int around() :  call(int Test.* (..)) //adviceexecution() && within(Plain2)
 	{
 		class LocalClass2 
