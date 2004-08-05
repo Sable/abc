9cb1a6c86330952a30de7c59a1c622e14b8ff1d0
pavel <pavel@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-08-05 19:21:57 +0000

    Additional ajc 1.2 test files

---
+++ aop/abc/ajc-harness/tests/new/TestBackdoor.java
@@ -0,0 +1,9 @@
+import org.aspectj.testing.Tester;
+
+public class TestBackdoor {
+    public static void main(String[] args) {
+        for (char c = 'A'; c <= 'D'; c++) {
+    	    System.out.println(c);
+        }
+    }
+}
