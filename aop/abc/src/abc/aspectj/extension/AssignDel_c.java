fc752838b71b1697692249ae7f1633de67a75eee
pavel <pavel@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-08-16 16:31:57 +0000

    Clean up unnecessary implementation of MakesAspectMethods

--- aop/abc/src/abc/aspectj/extension/AssignDel_c.java
+++ aop/abc/src/abc/aspectj/extension/AssignDel_c.java
@@ -11,7 +11,7 @@ import abc.aspectj.visit.*;
 import polyglot.types.*;
 import polyglot.visit.*;
 
-public class AssignDel_c extends JL_c implements MakesAspectMethods
+public class AssignDel_c extends JL_c
 {
 	
 	public Node typeCheck(TypeChecker tc) throws SemanticException {
@@ -21,20 +21,5 @@ public class AssignDel_c extends JL_c implements MakesAspectMethods
 			throw new SemanticException("Cannot assign to a local within a pointcut.", node().position());
 		return node().typeCheck(tc);
 	}
-	
-    public void aspectMethodsEnter(AspectMethods visitor)
-    {
-        visitor.pushLhs(((Assign) node()).left());
-    }
-
-    public Node aspectMethodsLeave(AspectMethods visitor, AJNodeFactory nf,
-                                   AJTypeSystem ts)
-    {
-        // TODO: Possibly we can remove the MakesAspectMethods interface.
-        Node n = node();
-        Expr oldleft = visitor.lhs();
-        visitor.popLhs();
-        return n;
-    }
 }
 
