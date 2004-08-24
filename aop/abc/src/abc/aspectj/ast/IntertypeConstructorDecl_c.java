559e7529200b70e15cc5123ddf371a720fea50c8
ganesh <ganesh@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-08-24 12:44:43 +0000

    copyright statements

--- aop/abc/src/abc/aspectj/ast/IntertypeConstructorDecl_c.java
+++ aop/abc/src/abc/aspectj/ast/IntertypeConstructorDecl_c.java
@@ -1,3 +1,23 @@
+/* Abc - The AspectBench Compiler
+ * Copyright (C) 2004 Aske Simon Christensen
+ * Copyright (C) 2004 Oege de Moor
+ *
+ * This compiler is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this compiler, in the file LESSER-GPL; 
+ * if not, write to the Free Software Foundation, Inc., 
+ * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
 package abc.aspectj.ast;
 
 import java.util.Iterator;
@@ -96,7 +116,7 @@ public class IntertypeConstructorDecl_c extends ConstructorDecl_c
     protected InterTypeConstructorInstance_c itConstructorInstance;
     
     /**
-     * @author Aske Christensen
+     * @author Aske Simon Christensen
      * @author Oege de Moor
      * add itd of methods to host types
      */
