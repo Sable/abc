c33783e193cfbd0365849b82dc7c758e693c954d
ganesh <ganesh@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-08-24 14:42:49 +0000

    made the @author finding regexp more forgiving

--- aop/abc/src/abc/main/Usage.java
+++ aop/abc/src/abc/main/Usage.java
@@ -1,4 +1,5 @@
 /* Abc - The AspectBench Compiler
+ * Copyright (C) 2004 Laurie Hendren
  *
  * This compiler is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
