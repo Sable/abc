0745dbc48337efda7043bdf5431a6efd75ee9fd9
dsereni <dsereni@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-08-25 10:25:51 +0000

    A very basic Cflow intraprocedural analysis to cut down on the number of locals and avoid repeated currentThread()s

---
+++ aop/abc/src/abc/weaving/weaver/CflowIntraAggregate.java
@@ -0,0 +1,196 @@
+package abc.weaving.weaver;
+
+import java.util.*;
+
+import soot.*;
+import soot.util.*;
+import soot.jimple.*;
+import abc.soot.util.LocalGeneratorEx;
+
+
+/** Cflow Intraprocedural analysis phase I:
+ *  aggregates all the local variables that access the same
+ *  CflowStack/Counter field of an aspect into one variable,
+ *  assigned at the beginning of the method.
+ *  Improves the bytecode slightly over what is otherwise produced.
+ *  Relies on: Any local variable of type CFlowStack/CFlowCounter is
+ *  introduced by the weaver, and the CFlowStack/CFlowCounter static
+ *  fields in aspects are never modified after they are initialized. 
+ * 
+ * @author Damien Sereni
+ */
+public class CflowIntraAggregate extends BodyTransformer {
+
+	// Number of different locals representing the same cflow stack/counter
+	// that we require before aggregating them
+	private final int AGGREGATE_THRESHOLD = 2;
+
+	private LocalGeneratorEx lg;
+
+	private SootClass cflowStackClass;
+	private SootClass cflowCounterClass;
+
+	private static CflowIntraAggregate instance = 
+		new CflowIntraAggregate();
+	private CflowIntraAggregate() {
+		// Get the SootClass objects for CflowStack and CflowCounter
+		this.cflowStackClass = Scene.v().getSootClass(
+			"uk.ac.ox.comlab.abc.runtime.internal.CFlowStack");
+		this.cflowCounterClass = Scene.v().getSootClass(
+			"uk.ac.ox.comlab.abc.runtime.internal.CFlowCounter");
+	}
+	
+	public static CflowIntraAggregate v() { return instance; }
+
+	private void addToMultimap(Map/*<SootFieldRef, Chain<Local>>*/ m, 
+							   SootFieldRef fr,
+							   Local l) {
+		if (m.containsKey(fr)) {
+			Chain oldchain = (Chain)m.get(fr);
+			oldchain.add(l);			   
+		} else {
+			HashChain newchain = new HashChain();
+			newchain.add(l);
+			m.put(fr, newchain);
+		}
+	}
+
+	private Map/*<SootFieldRef,Chain<Local>>*/ getCflowStackLocals(Body b) {
+		HashMap/*<SootFieldRef,Chain<Local>>*/ m = new HashMap();
+		Iterator units = b.getUnits().iterator();
+		while (units.hasNext()) {
+			Stmt s = (Stmt)units.next();
+			// Looking for:
+			// LOCAL<type: CflowStack or CflowCounter> := STATIC FIELD REF
+			if (s instanceof AssignStmt) {
+				Value left = ((AssignStmt)s).getLeftOp();
+				Value right = ((AssignStmt)s).getRightOp();
+				if (left instanceof Local) {
+				if (   left.getType().equals(cflowStackClass.getType()) 
+				    ||  left.getType().equals(cflowCounterClass.getType())) {
+				    	// We have an assignment to a stack/counter variable
+				    	// There should be no others, but just check anyway that
+				    	// we have a static field ref
+				    	if (right instanceof FieldRef) {
+				    		SootFieldRef fr = ((FieldRef)right).getFieldRef();
+				    		addToMultimap(m, fr, (Local)left);
+				    	}
+				    }
+				}
+			}
+		}
+		return m;
+	}
+
+	private boolean shouldAggregate(Chain/*<Local>*/ ls) {
+		return (ls.size() >= AGGREGATE_THRESHOLD);
+	}
+
+	private String getLocalName(SootFieldRef sf) {
+		if (sf.type().equals(cflowStackClass.getType())) {
+			return "cflowstack";
+		}
+		if (sf.type().equals(cflowCounterClass.getType())) {
+			return "cflowcounter";
+		}
+		throw new RuntimeException("Unknown Static Field Ref type: "+sf);
+	}
+
+	private Local addNewCflowLocalAndInit(Body b, Chain/*<Local>*/ ls, SootFieldRef sf) {
+		Local l = lg.generateLocal(sf.type(), getLocalName(sf));	
+		
+		Stmt assign = Jimple.v().newAssignStmt(l, Jimple.v().newStaticFieldRef(sf));
+		
+		b.getUnits().addFirst(assign);
+		
+		return l;
+	}
+
+	private void replaceCflowLocal(ValueBox e, Local newl) {
+		// Replace the base of a virtual invoke stmt by newl
+		
+		VirtualInvokeExpr vie = (VirtualInvokeExpr)e.getValue();
+		VirtualInvokeExpr newvie = 
+			Jimple.v().newVirtualInvokeExpr(newl, vie.getMethodRef(), vie.getArgs());
+		e.setValue(newvie);
+	}
+
+	private void replaceCflowLocalIfApplicable(ValueBox e, 
+											   Chain/*<Local>*/ ls,
+											   Local newl)
+	{
+		// Replace the base of a virtual invoke stmt by newl 
+		// PROVIDED the base was a local var in ls
+		
+		Value base = ((VirtualInvokeExpr)e.getValue()).getBase();
+		
+		Iterator it = ls.iterator();
+		while (it.hasNext()) {
+			Local l = (Local)it.next();
+			if (l.equivTo(base)) {
+				replaceCflowLocal(e, newl);
+				return;
+			}
+		}
+	}
+
+	private void findAndReplaceCflowLocals(Body b, Chain/*<Local>*/ ls, Local newl) {
+		Iterator units = b.getUnits().iterator();
+		
+		while (units.hasNext()) {
+			Stmt s = (Stmt)units.next();
+			
+			if (s.containsInvokeExpr()) {
+				InvokeExpr e = s.getInvokeExpr();
+				if (e instanceof VirtualInvokeExpr) {
+					replaceCflowLocalIfApplicable(s.getInvokeExprBox(),ls,newl);
+				}
+			}
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see soot.BodyTransformer#internalTransform(soot.Body, java.lang.String, java.util.Map)
+	 */
+	protected void internalTransform(Body b, String phaseName, Map options) {
+		// Initialise a local var generator:
+		
+		lg = new LocalGeneratorEx(b);
+		
+		// Find all the locals that get assigned cflow stacks/counters, and produce
+		// the mapping from cflow stacks/counters to sets of variables that represent
+		// them
+		
+		Map/*<SootFieldRef,Chain<Local>>*/ cflows = getCflowStackLocals(b);
+		
+		// FIXME Check that this doesn't cause a problem with the initial setting of 
+		// the CflowStack/Counter field in the aspect
+		
+		// For each stack/counter, if we bother to aggregate, make a new var to hold the
+		// stack/counter, and assign to it at the beginning of the method
+		// TODO Be more clever and assign to it at the latest common ancestors of all references
+		// Then replace all references to any of the vars it subsumes by that var
+		//System.out.println("******* " + b.getMethod().getName());
+		Iterator it = cflows.keySet().iterator();
+		while (it.hasNext()) {
+			SootFieldRef sf = (SootFieldRef)it.next();
+			Chain/*<Local>*/ ls = (Chain/*<Local>*/)cflows.get(sf);
+			if (shouldAggregate(ls)) {
+				//System.out.println("Found something to aggregate: " + sf + " with " + 
+		//		ls.size() + " uses");
+				
+				// Add the new local and initialisation
+				Local newl = addNewCflowLocalAndInit(b, ls, sf);
+				//System.out.println("Aggregated to " + newl);
+				
+				// Replace other locals
+				
+				findAndReplaceCflowLocals(b, ls, newl);
+			}
+		}
+		
+		// TODO Check that useless locals do get thrown away after this!
+
+	}
+
+}
---
+++ aop/abc/src/abc/weaving/weaver/CflowIntraproceduralAnalysis.java
@@ -0,0 +1,310 @@
+package abc.weaving.weaver;
+
+import abc.soot.util.LocalGeneratorEx;
+import java.util.*;
+import soot.*;
+import soot.util.*;
+import soot.jimple.*;
+
+/**
+ * @author Damien Sereni
+ *
+ * A simple intraprocedural analysis to avoid having to find the current
+ * thread more than once for CflowStack / CflowCounter updating
+ */
+
+public class CflowIntraproceduralAnalysis extends BodyTransformer {
+
+	// The sharing threshold: the number of invocations of methods on a
+	// single cflow stack or counter required for us to get the right 
+	// object in advance to avoid getting the current thread over and 
+	// over again
+	// Trade-off: currently the thread get will occur at the beginning of
+	// the method, regardless of whether the cflow operations happen or not
+	// So can make it slower - should only do this if we're reasonably certain
+	// that the number of invokes makes it (possibly) worthwhile
+	// Also, adds a local per cflow stack or counter
+
+	private final int SHARE_THRESHOLD = 2;
+
+	private LocalGeneratorEx lg;
+
+	private Map/*<String,String>*/ cflowCounterRename;
+	private Map/*<String,String>*/ cflowStackRename;	
+	
+	private SootClass cflowStackClass;
+	private SootClass cflowCounterClass;
+	private Type objectType;
+
+	private static CflowIntraproceduralAnalysis instance = 
+		new CflowIntraproceduralAnalysis();
+	private CflowIntraproceduralAnalysis() {
+		// Get the SootClass objects for CflowStack and CflowCounter
+		this.cflowStackClass = Scene.v().getSootClass(
+			"uk.ac.ox.comlab.abc.runtime.internal.CFlowStack");
+		this.cflowCounterClass = Scene.v().getSootClass(
+			"uk.ac.ox.comlab.abc.runtime.internal.CFlowCounter");
+		this.objectType = Scene.v().getSootClass(
+			"java.lang.Object").getType();
+			
+		// Initialise the renaming cflowRename
+		// Maps a CflowCounter/CflowStack method name
+		// to the name of the equivalent method taking an 
+		// additional parameter (the cached stack/counter)
+		this.initCflowRename();
+	}
+	
+	private void initCflowRename() {
+		cflowCounterRename = new HashMap();
+		cflowStackRename = new HashMap();
+		
+		// CFlowCounter Methods
+		cflowCounterRename.put("inc", "incCounter");
+		cflowCounterRename.put("dec", "decCounter");
+		cflowCounterRename.put("isValid", "isValidCounter");
+		
+		// CFlowStacke Methods
+		cflowStackRename.put("push", "pushStack");
+		cflowStackRename.put("pushInstance", "pushInstanceStack");
+		cflowStackRename.put("pop", "popStack");
+		
+		cflowStackRename.put("peek", "peekStack");
+		cflowStackRename.put("get", "getTopStack");
+		cflowStackRename.put("peekInstance", "peekInstanceStack");
+		cflowStackRename.put("peekCFlow", "peekCFlowStack");
+		cflowStackRename.put("peekTopCFlow", "peekTopCFlowStack");
+		
+		cflowStackRename.put("isValid", "isValidStack");
+	}
+	
+	public static CflowIntraproceduralAnalysis v() { return instance; }
+
+	private Chain/*<Local>*/ getCflowLocals(Body b) {
+		Chain/*<Local>*/ locals = b.getLocals();
+		Chain /*<Local>*/ cflowLocals = new HashChain();
+		
+		Iterator it = locals.iterator();
+		while (it.hasNext()) {
+			Local l = (Local)it.next();
+			if (l.getType().equals(cflowStackClass.getType())) {
+				cflowLocals.add(l);
+			}
+			if (l.getType().equals(cflowCounterClass.getType())) {
+				cflowLocals.add(l);
+			}
+		}
+		
+		return cflowLocals;
+	}
+
+	private Chain/*<ValueBox>*/ getCflowLocalStmts(Local l, Body b) {
+		PatchingChain units = b.getUnits();
+		Chain cflowstmts = new HashChain();
+		
+		Iterator it = units.iterator();
+		while (it.hasNext()) {
+			Stmt s = (Stmt)it.next();
+			
+			if (s.containsInvokeExpr()) {
+				InvokeExpr e = s.getInvokeExpr();
+				if (e instanceof VirtualInvokeExpr) {
+					Value v = ((VirtualInvokeExpr)e).getBase();
+					if (l.equivTo(v)) {
+						cflowstmts.add(s.getInvokeExprBox());
+					}
+				}
+			}
+		}
+		return cflowstmts;
+	}
+
+	private boolean shouldShareStack(Local l, Chain/*<ValueBox>*/ invokes, Body b) {
+		return (invokes.size() >= SHARE_THRESHOLD);
+	}
+
+	private Stmt findInitStmt(Local cflowLoc, Body b) {
+		// Find the initialising statement for cflowLoc
+		
+		Iterator it = b.getUnits().iterator();
+		while (it.hasNext()) {
+			Stmt s = (Stmt)it.next();
+			if (s instanceof AssignStmt) {
+				Value base = ((AssignStmt)s).getLeftOp();
+				if (cflowLoc.equivTo(base))
+				 	return s;
+			}
+		}
+		throw new RuntimeException("Could not find statement initialising "+cflowLoc.getName());
+	}
+
+	private Local addThreadLocalAndInitialise(Local cflowLoc, Body b) {
+		Local threadl = lg.generateLocal(objectType, cflowLoc.getName()+"Cache");
+		
+		if (cflowLoc.getType().equals(cflowStackClass.getType())) {
+			SootMethodRef stackGetThread = 
+				Scene.v().makeMethodRef(cflowStackClass, 
+										"getStack",
+										new ArrayList(),
+										objectType,
+										false);
+			Stmt init = 
+				Jimple.v().newAssignStmt(threadl, 
+				Jimple.v().newVirtualInvokeExpr(cflowLoc, 
+												stackGetThread,
+												new ArrayList()));
+			
+			b.getUnits().insertAfter(init,
+									 findInitStmt(cflowLoc, b));
+		} else 
+		if (cflowLoc.getType().equals(cflowCounterClass.getType())) {
+			SootMethodRef counterGetThread = 
+							Scene.v().makeMethodRef(cflowCounterClass, 
+													"getCounter",
+													new ArrayList(),
+													objectType,
+													false);
+			Stmt init = 
+				Jimple.v().newAssignStmt(threadl, 
+				Jimple.v().newVirtualInvokeExpr(cflowLoc, 
+												counterGetThread,
+												new ArrayList()));
+			
+			b.getUnits().insertAfter(init,
+									 findInitStmt(cflowLoc, b));
+		} else
+			throw new RuntimeException("Cflow local "+cflowLoc+
+					" has unexpected type "+cflowLoc.getType());
+
+		return threadl;
+	}
+
+	private String getNewName(boolean isStack, String oldname) {
+		String newname = (String) 
+			(isStack?cflowStackRename:cflowCounterRename)
+				.get(oldname);
+		
+		if (newname == null) {
+			throw new RuntimeException("Error: could not find "+
+			(isStack?"CFlowStack":"CFlowCounter")+" method "+oldname);
+		}
+		return newname;
+	}
+
+	private void updateCflowUse(ValueBox vb, Local threadl) {
+		// NOTE vb contains a VirtualInvokeExpr
+		VirtualInvokeExpr vie = (VirtualInvokeExpr)vb.getValue();
+		
+		boolean isStack;
+		
+		if (vie.getMethodRef().declaringClass().equals(cflowStackClass)) {
+			isStack = true;
+		} else
+		if (vie.getMethodRef().declaringClass().equals(cflowCounterClass)) {
+			isStack = false;
+		} else throw new RuntimeException("Unknown class in presumed cflow runtime operation: "+
+										  vie.getMethodRef().getClass());
+		
+		String newName = getNewName(isStack, vie.getMethodRef().name());
+						
+		List newParameterTypes = new ArrayList();
+		Iterator paramIt = vie.getMethodRef().parameterTypes().iterator();
+		while (paramIt.hasNext()) {
+			Type t = (Type)paramIt.next();
+			newParameterTypes.add(t);
+		}
+		newParameterTypes.add(objectType);
+		
+		SootMethodRef newMethod = Scene.v().makeMethodRef(
+			vie.getMethodRef().declaringClass(),
+			newName,
+			newParameterTypes,
+			vie.getMethodRef().returnType(),
+			vie.getMethodRef().isStatic());
+			
+		ArrayList newParams = new ArrayList();
+		Iterator actualIt = vie.getArgs().iterator();
+		while (actualIt.hasNext()) {
+			Value v = (Value)actualIt.next();
+			newParams.add(v);
+		}
+		newParams.add(threadl);
+			
+		VirtualInvokeExpr newvie = 
+			Jimple.v().newVirtualInvokeExpr(
+				(Local)vie.getBase(),
+				newMethod,
+				newParams);
+			
+		vb.setValue(newvie);
+	}
+
+	private void updateCflowUseIfNecessary(ValueBox vb, Local threadl) {
+		// Update a use of a cflow variable to use the cached value
+		// IF necessary - ie if the use is not the call to getStack/getCounter!
+		
+		SootMethodRef meth = ((VirtualInvokeExpr)vb.getValue()).getMethodRef();
+		
+		if (meth.name().equals("getStack") || meth.name().equals("getCounter"))
+			return;
+		
+		updateCflowUse(vb, threadl);
+	}
+
+	private void updateCflowUses(Local cflowl, Local threadl, Body b) {
+		Iterator it = b.getUnits().iterator();
+		
+		while (it.hasNext()) {
+			Stmt s = (Stmt)it.next();
+			if (s.containsInvokeExpr()) {
+				InvokeExpr e = s.getInvokeExpr();
+				if (e instanceof VirtualInvokeExpr) {
+					Value base = ((VirtualInvokeExpr)e).getBase();
+					if (cflowl.equivTo(base)) {
+						updateCflowUseIfNecessary(s.getInvokeExprBox(), threadl);
+					}
+				}
+			}
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see soot.BodyTransformer#internalTransform(soot.Body, java.lang.String, java.util.Map)
+	 */
+	protected void internalTransform(Body b, String phaseName, Map options) {
+		// Get a new generator for local vars
+		lg = new LocalGeneratorEx(b);
+		
+		// Get a list of CflowStacks/CflowCounters
+	
+		Chain/*<Local>*/ cflowLocals = getCflowLocals(b);
+		
+		// For each CflowStack/Counter, get all occurences (Stmts)
+		
+		// Iterate through the stacks/counters, if the # of occurences is
+		// large enough, 
+		//    - add a local to hold the thread stack/counter
+		//    - add a statement to initialise it at beginning of body
+		// TODO find a better place to initialise the thread stack/counter
+		// (as may not always need to initialise it!)
+		//    - change all statements to use the saved stack/counter
+
+		Iterator it = cflowLocals.iterator();
+		while (it.hasNext()) {
+			Local l = (Local)it.next(); 
+			Chain/*<ValueBox>*/ invokes = getCflowLocalStmts(l, b);
+			if (shouldShareStack(l, invokes, b)) {
+				//System.out.println("Found something to share!: " +
+				//	l + " with " + invokes.size() + " statements");
+				// Add a local and initialise it
+				
+				Local threadl = addThreadLocalAndInitialise(l, b);
+				
+				// Change all statements
+				
+				updateCflowUses(l, threadl, b);
+			} 
+		}
+
+	}
+
+}
