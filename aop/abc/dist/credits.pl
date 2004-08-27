fda8bc993314cc56dc62037f553d66f80cbdc379
ganesh <ganesh@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-08-27 12:43:38 +0000

    bits and pieces

---
+++ aop/abc/dist/credits.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+print << 'START';
+<!--#set var="title" value="Credits"-->
+<!--#set var="file" value="credits.shtml"-->
+<!--#set var="logo" value="standard"-->
+
+<!--#include file="includes/header.shtml"-->
+<div class="main">
+START
+
+my $indashlist=0;
+my $inpara=0;
+
+while(<>) {
+  s/\</&lt;/g; s/\</&gt;/g;
+  s{http://([\w\d\.\/]+)}{<a href=\"http://$1\">http://$1</a>}g;
+  if($_=~/^\s+\-(.*)$/) {
+    if(!$indashlist) { 
+      $indashlist=1;
+      $inpara=0;
+      print "</p><ul><li>\n"; 
+    }
+    else { print "</li><li>\n"; }
+    print "$1\n";
+  } elsif($_=~/^\s+$/) {
+    if($indashlist) {
+      print "</li></ul>\n";
+      $indashlist=0;
+    }
+    if($inpara) {
+      print "</p>\n";
+      $inpara=0;
+    }
+  } else {
+    if(!$inpara && !$indashlist) {
+      print "<p>\n";
+      $inpara=1;
+    }
+    print $_;
+  }
+}
+if($indashlist) {
+  print "</li></ul>\n";
+}
+if($inpara) {
+  print "</p>\n";
+}
+
+
+print << 'END';
+</div>
+
+<!--#include file="includes/footer.shtml"-->
+END
+
--- aop/abc/dist/package.sh
+++ aop/abc/dist/package.sh
@@ -1,11 +1,21 @@
 #!/bin/sh -e
 
+# Version of the package-body.shtml file to target; update this if you
+# make an incompatible change in that file
+PACKAGE='1'
+# The Jedd runtime version
+JEDD='0.2'
+# The Xact version
+XACT='1.0-1'
+
+DATE=`date +%Y%m%d%H%M%S`
+
 case "$1" in
   release)
 	PREREL=
 	;;
   snapshot)
-	PREREL=`date +%Y%m%d%H%M%S`
+	PREREL=$DATE
 	;;
   *)
 	echo "Please select one of 'snapshot' or 'release'"
@@ -16,7 +26,7 @@ ant clobber
 mkdir -p classes/abc/main
 echo "prerelease=$PREREL" > classes/abc/main/version.properties
 # FIXME : URL refs for javadoc
-ant jars javadoc runtime-javadoc
+ant jars javadoc runtime-javadoc copy-jars
 rm classes/abc/main/version.properties
 
 VERSION=`CLASSPATH=lib/abc-complete.jar java abc.aspectj.Version`
@@ -25,19 +35,21 @@ rm -rf package/abc-$VERSION
 
 mkdir -p package/abc-$VERSION
 mkdir -p package/abc-$VERSION/bin
-cp -a build.xml lib/ src/ runtime-src/ testing-src/ ajc-harness/ javadoc/ runtime-javadoc/ package/abc-$VERSION/
+cp -a build.xml CREDITS LESSER-GPL CPL LICENSING ant.settings.template lib/ src/ runtime-src/ testing-src/ ajc-harness/ javadoc/ runtime-javadoc/ package/abc-$VERSION/
 cp dist/abc package/abc-$VERSION/bin/
+
+
 cd package
 
 BINS="\
    abc-$VERSION/bin/abc \
-   abc-$VERSION/lib/abc-complete.jar \
-   abc-$VERSION/lib/abc-runtime.jar \
-   abc-$VERSION/lib/abc-for-ajc-ant.jar \
+   abc-$VERSION/lib \
    abc-$VERSION/runtime-javadoc/ \
 "
 
 SRCS="\
+   abc-$VERSION/build.xml \
+   abc-$VERSION/ant.settings.template \
    abc-$VERSION/src/ \
    abc-$VERSION/runtime-src/ \
    abc-$VERSION/testing-src/ \
@@ -47,21 +59,77 @@ SRCS="\
 
 rm -f abc-$VERSION-bin.*
 rm -f abc-$VERSION-src.*
-tar -czvf abc-$VERSION-bin.tar.gz $BINS
+tar --exclude-from ../dist/tar-excludes -czvf abc-$VERSION-bin.tar.gz $BINS
 tar --exclude-from ../dist/tar-excludes -czvf abc-$VERSION-src.tar.gz $SRCS
 
-mkdir tmp
-cd tmp
-tar -xzvf ../abc-$VERSION-bin.tar.gz
-zip -r ../abc-$VERSION-bin.zip abc-$VERSION/
-cd ..
-rm -rf tmp
+rm -rf soot-dev-$DATE/
+rm -f soot-dev-$DATE.tar.gz soot-dev-$DATE.zip
+svn export /usr/local/src/soot-dev soot-dev-$DATE
+tar -czvf soot-dev-$DATE.tar.gz soot-dev-$DATE/
 
-mkdir tmp
-cd tmp
-tar -xzvf ../abc-$VERSION-src.tar.gz
-zip -r ../abc-$VERSION-src.zip abc-$VERSION/
-cd ..
-rm -rf tmp
+rm -rf jasmin-dev-$DATE/
+rm -f jasmin-dev-$DATE.tar.gz jasmin-dev-$DATE.zip
+svn export /usr/local/src/jasmin-dev jasmin-dev-$DATE
+tar -czvf jasmin-dev-$DATE.tar.gz jasmin-dev-$DATE/
+
+rm -rf polyglot-dev-$DATE/
+rm -f polyglot-dev-$DATE.tar.gz polyglot-dev-$DATE.zip
+cp -a /usr/local/src/polyglot-dev ./polyglot-dev-$DATE
+# only a rough approximation of what's needed...
+tar --exclude CVS --exclude update.sh --exclude lib/ --exclude classes/ \
+    --exclude cup-classes/ --exclude \*.class \
+    -czvf polyglot-dev-$DATE.tar.gz polyglot-dev-$DATE/
+
+for d in abc-$VERSION-bin abc-$VERSION-src soot-dev-$DATE polyglot-dev-$DATE \
+         jasmin-dev-$DATE
+ do mkdir tmp
+    cd tmp
+    tar -xzvf ../$d.tar.gz
+    zip -r ../$d.zip *
+    cd ..
+    rm -rf tmp
+done
+
+mkdir -p ../dists
+
+rm -rf ../dists/$VERSION
+mkdir -p ../dists/$VERSION/files
+for root in abc-$VERSION-bin abc-$VERSION-src soot-dev-$DATE \
+            polyglot-dev-$DATE jasmin-dev-$DATE
+ do cp $root.tar.gz $root.zip ../dists/$VERSION/files/
+done
+
+mkdir ../dists/$VERSION/files/lib
+cp abc-$VERSION/lib/*.jar ../dists/$VERSION/files/lib/
+cp /usr/local/src/xact/java/xact-complete.jar \
+    ../dists/$VERSION/files/lib/xact-complete-$XACT.jar
+cp /usr/local/src/soot-dev/lib/jedd-runtime-$JEDD.jar \
+    ../dists/$VERSION/files/lib/jedd-runtime-$JEDD.jar
+
+mkdir ../dists/$VERSION/files/bin
+cp abc-$VERSION/bin/abc ../dists/$VERSION/files/bin
+
+cp -a abc-$VERSION/runtime-javadoc ../dists/$VERSION/files/
+
+echo "<!--#set var=\"version\" value=\"$VERSION\"-->" \
+    > ../dists/$VERSION/package.shtml
+echo "<!--#set var=\"sootversion\" value=\"dev-$DATE\"-->" \
+    >> ../dists/$VERSION/package.shtml
+echo "<!--#set var=\"jasminversion\" value=\"dev-$DATE\"-->" \
+    >> ../dists/$VERSION/package.shtml
+echo "<!--#set var=\"polyglotversion\" value=\"dev-$DATE\"-->" \
+    >> ../dists/$VERSION/package.shtml
+echo "<!--#set var=\"jeddversion\" value=\"$JEDD\"-->" \
+    >> ../dists/$VERSION/package.shtml
+echo "<!--#set var=\"xactversion\" value=\"$XACT\"-->" \
+    >> ../dists/$VERSION/package.shtml
+echo "<!--#include file=\"package-body-v$PACKAGE.shtml\"-->" \
+    >> ../dists/$VERSION/package.shtml
+
+cd ../dists/$VERSION/
+ln -sf ../../includes/package-body-v$PACKAGE.shtml
+ln -sf ../../includes/footer.shtml
+ln -sf ../../includes/header.shtml
 
 cd ..
+
