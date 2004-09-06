cd04a4d2da609a4a953aad72808f06ba4b9606e3
ganesh <ganesh@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-09-06 17:33:22 +0000

    new test

---
+++ aop/abc/ajc-harness/tests/abctests/bugs/DeclareParentsInner.java
@@ -0,0 +1,12 @@
+// bug with abc where the declare parents is required to make the
+// inner class reference in B be found, but it isn't
+
+class A {
+    class Inner {}
+}
+class B {
+    void foo(Inner x);
+}
+public aspect DeclareParentsInner {
+    declare parents : B extends A;
+}
