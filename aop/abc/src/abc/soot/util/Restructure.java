feaae36fbe0ef7c81945c01f2912de9fc7480fd5
ganesh <ganesh@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-09-10 12:48:13 +0000

    copyright notice spaces update

--- aop/abc/src/abc/soot/util/Restructure.java
+++ aop/abc/src/abc/soot/util/Restructure.java
@@ -16,8 +16,8 @@
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with this compiler, in the file LESSER-GPL; 
- * if not, write to the Free Software Foundation, Inc., 
+ * License along with this compiler, in the file LESSER-GPL;
+ * if not, write to the Free Software Foundation, Inc.,
  * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
