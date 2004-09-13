d36fc097ff07f9acb21ab763ef32a0e46220ec9d
ganesh <ganesh@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-09-13 12:44:49 +0000

    more distributing stuff

---
+++ aop/abc/dist/distribute.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -e
+
+if [ "$1" = "" ]
+  then echo "Please specify a version number"
+       exit 1
+fi
+
+
+rm -rf /srv/www/abc.comlab.ox.ac.uk/html/dists/$1
+cp -a dists/$1 /srv/www/abc.comlab.ox.ac.uk/html/dists/
+
+rm -f /srv/www/abc.comlab.ox.ac.uk/apt/dists/abc/main/binary-all/abc_${1}_all.deb
+ln -s \
+ /srv/www/abc.comlab.ox.ac.uk/html/dists/$1/files/debian/abc_${1}_all.deb \
+ /srv/www/abc.comlab.ox.ac.uk/apt/dists/abc/main/binary-all/
+
+/srv/www/abc.comlab.ox.ac.uk/scripts/build-apt.sh abc main
+dist/credits.pl < CREDITS > /srv/www/abc.comlab.ox.ac.uk/html/credits.shtml
+
+echo Done.
