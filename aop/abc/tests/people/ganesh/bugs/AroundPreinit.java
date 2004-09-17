b6e0e5b028b7249b8e67fcfafc620f270415c27d
ganesh <ganesh@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-09-17 20:56:07 +0000

    foo

--- aop/abc/tests/people/ganesh/bugs/AroundPreinit.java
+++ aop/abc/tests/people/ganesh/bugs/AroundPreinit.java
@@ -2,16 +2,23 @@
 public class AroundPreinit {
    public static void main(String[] args) {
       new C();
-   } 
+   }
 }
 
-class C { C() { } }
-
-aspect A {
-    void around(): staticinitialization(C) {
-       proceed();
+class C {
+    C() {
+        this(foo());
     }
 
+    C(int x) {
+    }
+
+    static int foo() {
+        return 5;
+    }
+}
+
+aspect A {
     void around(): preinitialization(C.new()) {
        proceed();
     }
