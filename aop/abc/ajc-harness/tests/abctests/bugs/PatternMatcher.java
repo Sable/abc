289c74874ff4cc135afa33cf7daf10a8bdf20065
ganesh <ganesh@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-10-05 17:40:07 +0000

    test case

---
+++ aop/abc/ajc-harness/tests/abctests/bugs/PatternMatcher.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class PatternMatcher {
+
+    public boolean matchesClass() {
+        return false;
+    }
+
+    private class AIMethodPattern
+      {
+        public boolean matchesExecution() {
+            LinkedList ftypes = null;
+
+            int skip_last = 0;
+
+            while (skip_last-- > 0) ftypes.removeLast();
+
+            return matchesClass();
+        }
+
+    }
+
+    public static void main(String[] args) {}
+
+
+}
