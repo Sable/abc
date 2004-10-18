b6024e15aed21566b3c29afbc00be4489c4c9038
ganesh <ganesh@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-10-18 23:45:15 +0000

    null residue check

---
+++ aop/abc/aspects/NullResidue.java
@@ -0,0 +1,7 @@
+import polyglot.util.InternalCompilerError;
+
+public aspect NullResidue {
+    after returning(Residue r):execution(* *.*(..)) {
+        throw new InternalCompilerError("null residue returned");
+    }
+}
