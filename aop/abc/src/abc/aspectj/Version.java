fa19f8bc0630bf7467801fb0f20295d6687aa89c
ganesh <ganesh@40514614-586c-40e6-bf05-bf7e477dc3e6>
2004-10-22 11:06:17 +0000

    1.0.0

--- aop/abc/src/abc/aspectj/Version.java
+++ aop/abc/src/abc/aspectj/Version.java
@@ -31,9 +31,9 @@ import java.util.Properties;
 public class Version extends polyglot.main.Version {
     public String name() { return "aspectj"; }
 
-    public int major() { return 0; }
-    public int minor() { return 9; }
-    public int patch_level() { return 1; }
+    public int major() { return 1; }
+    public int minor() { return 0; }
+    public int patch_level() { return 0; }
 
     protected String properties_file() {
 	return "/abc/main/version.properties";
