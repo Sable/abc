eb75404dd2d13d9f11f249f253fb9b1d4a5345fb
c03sk <c03sk@40514614-586c-40e6-bf05-bf7e477dc3e6>
2005-01-13 15:30:22 +0000

    *** empty log message ***

---
+++ aop/abc/tests/people/sascha/java15/Foo.java
@@ -0,0 +1,52 @@
+
+
+class Container<E> {
+    
+    public void set(E val) { 
+	this.val=val;
+	// fails: Aspect.pc2(this); 
+	Aspect.pc3(this);
+    }
+
+  public void set(Container<? extends E> val) { set(val.get()); }
+  
+  public E get() { return val; }
+    private E val;
+}
+
+public class Foo {
+ public static void main(String[] args) {
+   Container<String> c=new Container<String>();
+   c.set("hello"); Aspect.pc1(c); 
+
+   String s=c.get(); Aspect.pc4(c.get(), c); Aspect.pc5(c); Aspect.pc6(c); Aspect.pc7(c);
+ }
+}
+
+/*
+aspect Aspect {
+*/
+class Aspect {
+    // pointcut pc1(): call(* Container<String>.set(..)) {}
+    static public void pc1(Container<String> c) {  }
+
+    // pointcut pc2(): execution(* Container<String>.set(..)) {}
+    static public void pc2(Container<String> c) {  }
+
+    // pointcut pc3(): execution(* Container<?>.set(..)) {}
+    static public void pc3(Container<?> c) {  }
+
+    // pointcut pc4(): call(String Container<?>.get(..)) {}
+    static public void pc4(String s, Container<?> c) {  }
+
+    // <T extends Container> pointcut pc5(): call(* T.get(..)) {}
+    static public <T extends Container> void pc5(T c) {  }
+
+    
+    // <E, T extends Container<E>> pointcut pc6(): call(* T.get(..)) {}
+    static public <E, T extends Container<E>> void pc6(T c) { }
+   
+     // <E extends String, T extends Container<E>> pointcut pc7(): call(* T.get(..)) {}
+    static public <E extends String, T extends Container<E>> void pc7(T c) { }
+   
+}
