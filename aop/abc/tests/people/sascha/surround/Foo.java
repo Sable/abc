6d13fa978c7b4a75d750b6da3347dd08ead17bc3
c03sk <c03sk@40514614-586c-40e6-bf05-bf7e477dc3e6>
2005-02-09 15:36:44 +0000

    *** empty log message ***

--- aop/abc/tests/people/sascha/surround/Foo.java
+++ aop/abc/tests/people/sascha/surround/Foo.java
@@ -4,13 +4,21 @@ public class Foo {
     public static void main(String[] args) {
 	new Foo().foo();
     }
-    public void foo() { }
+    public void foo() { System.out.println("foo"); }
 }
 
 aspect Bar {
-    surround() : if(true) {
+    surround() : call(* foo()) {
+	System.out.println("surround before");
+    } {
+	System.out.println("surround after");
+    }
 
-    } {}
+    surround() : call(* foo()) {
+	System.out.println("surround 2 before");
+    } {
+	System.out.println("surround 2 after");
+    }
 
   
 }
