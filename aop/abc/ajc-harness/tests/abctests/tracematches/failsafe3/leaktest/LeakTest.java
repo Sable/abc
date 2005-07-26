ad0f3585b334f5dd24e5760a66585cacb8bb890c
oege <oege@40514614-586c-40e6-bf05-bf7e477dc3e6>
2005-07-26 11:59:20 +0000

    *** empty log message ***

---
+++ aop/abc/ajc-harness/tests/abctests/tracematches/failsafe3/leaktest/LeakTest.java
@@ -0,0 +1,34 @@
+package leaktest;
+
+import java.util.*;
+
+class MyEnum implements Enumeration {
+
+    int i;
+    Vector v;
+
+    MyEnum(Vector v) {
+	this.v = v;
+    }
+
+    public boolean hasMoreElements() {
+	return (i < v.size());
+    }
+
+    public Object nextElement() {
+        return v.get(i);
+    }
+}
+
+public class LeakTest {
+
+    public static void main(String[] args) {
+	    Vector v = new Vector(10);
+            Enumeration e1 = new MyEnum(v);
+            Enumeration e2 = new MyEnum(v);
+            v.add(new Object());
+	    // Enumeration e3 = new MyEnum(v);
+            e1.nextElement(); 
+	    e2.nextElement();
+    }
+}
