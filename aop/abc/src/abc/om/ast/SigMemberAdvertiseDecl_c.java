de60b430ce8a681340382e7159bec4eed848045f
neil <neil@40514614-586c-40e6-bf05-bf7e477dc3e6>
2005-09-13 16:42:31 +0000

    Changed syntax to match paper. Still uses old semantics and constraints.

---
+++ aop/abc/src/abc/om/ast/SigMemberAdvertiseDecl_c.java
@@ -0,0 +1,101 @@
+/* abc - The AspectBench Compiler
+ * Copyright (C) 2005
+ *
+ * This compiler is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This compiler is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this compiler, in the file LESSER-GPL;
+ * if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+/*
+ * Created on May 30, 2005
+ *
+ */
+package abc.om.ast;
+
+import java.util.Collections;
+import java.util.LinkedList;
+
+import abc.aspectj.ast.AJNodeFactory;
+import abc.aspectj.ast.MakesAspectMethods;
+import abc.aspectj.ast.MethodConstructorPattern;
+import abc.aspectj.ast.PCCall_c;
+import abc.aspectj.types.AJTypeSystem;
+import abc.aspectj.visit.AspectMethods;
+import abc.weaving.aspectinfo.Pointcut;
+import polyglot.ast.Node;
+import polyglot.ext.jl.ast.Node_c;
+import polyglot.util.CodeWriter;
+import polyglot.visit.NodeVisitor;
+import polyglot.visit.PrettyPrinter;
+
+/**
+ * @author Neil Ongkingco
+ *
+ */
+public class SigMemberAdvertiseDecl_c extends Node_c implements
+        SigMemberAdvertiseDecl, MakesAspectMethods {
+
+    private abc.aspectj.ast.Pointcut pc;
+    private boolean isPrivate = false;
+    
+    public SigMemberAdvertiseDecl_c(polyglot.util.Position pos, 
+            abc.aspectj.ast.Pointcut pc, boolean isPrivate) {
+        super(pos);
+        //store the call pointcut that represents the method
+        this.pc = pc;
+        this.isPrivate = isPrivate;
+    }
+    
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+
+    public void prettyPrint(CodeWriter w, PrettyPrinter pp) {
+        pc.prettyPrint(w, pp);
+        w.newline();
+        //super.prettyPrint(w, pp);
+    }    
+    
+    public SigMemberAdvertiseDecl_c reconstruct(abc.aspectj.ast.Pointcut pc) {
+        if (pc != this.pc) {
+            SigMemberAdvertiseDecl_c n = (SigMemberAdvertiseDecl_c)copy();
+            n.pc = pc;
+            return n;
+        }
+        return this;
+    }
+    
+    public Node visitChildren(NodeVisitor v) {
+        abc.aspectj.ast.Pointcut pc = 
+            (abc.aspectj.ast.Pointcut)visitChild(this.pc, v);
+        return reconstruct(pc);
+    }
+
+    /* (non-Javadoc)
+     * @see abc.openmod.ast.SigMember#getAIPointcut()
+     */
+    public Pointcut getAIPointcut() {
+        // Only returns the AI pointcut for the call pointcut
+        // The pass CheckModuleMembers should conjoin the !within pointcut
+        return this.pc.makeAIPointcut();
+    }
+    
+    public void aspectMethodsEnter(AspectMethods visitor) {
+        //push an empty list of formals. needed for If pointcuts
+        visitor.pushFormals(Collections.unmodifiableList(new LinkedList()));
+    }
+    public Node aspectMethodsLeave(AspectMethods visitor, AJNodeFactory nf,
+            AJTypeSystem ts) {
+        return this;
+    }
+}
---
+++ aop/abc/src/abc/om/ast/SigMemberExposeDecl_c.java
@@ -0,0 +1,93 @@
+/* abc - The AspectBench Compiler
+ * Copyright (C) 2005
+ *
+ * This compiler is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This compiler is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this compiler, in the file LESSER-GPL;
+ * if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+/*
+ * Created on May 13, 2005
+ *
+ */
+package abc.om.ast;
+
+import java.util.*;
+
+import abc.aspectj.ast.AJNodeFactory;
+import abc.aspectj.ast.MakesAspectMethods;
+import abc.aspectj.types.AJTypeSystem;
+import abc.aspectj.visit.AspectMethods;
+import abc.weaving.aspectinfo.Pointcut;
+import polyglot.ast.Node;
+import polyglot.ast.Term;
+import polyglot.ext.jl.ast.Node_c;
+import polyglot.ext.jl.ast.Term_c;
+import polyglot.util.CodeWriter;
+import polyglot.util.Position;
+import polyglot.visit.CFGBuilder;
+import polyglot.visit.NodeVisitor;
+import polyglot.visit.PrettyPrinter;
+
+/**
+ * @author Neil Ongkingco 
+ *
+ */
+public class SigMemberExposeDecl_c extends Node_c implements SigMemberExposeDecl, MakesAspectMethods {
+
+    private abc.aspectj.ast.Pointcut pc;
+    private boolean isPrivate = false;
+    
+    public SigMemberExposeDecl_c(polyglot.util.Position pos, abc.aspectj.ast.Pointcut pc, boolean isPrivate) {
+        super(pos);
+        this.pc = pc;
+        this.isPrivate = isPrivate;
+    }
+    
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+    
+    public Pointcut getAIPointcut() {
+        return this.pc.makeAIPointcut();
+    }
+    public void prettyPrint(CodeWriter w, PrettyPrinter pp) {
+        pc.prettyPrint(w, pp);
+        w.newline();
+        //super.prettyPrint(w, pp);
+    }    
+    
+    public SigMemberExposeDecl_c reconstruct(abc.aspectj.ast.Pointcut pc) {
+        if (pc != this.pc) {
+            SigMemberExposeDecl_c n = (SigMemberExposeDecl_c)copy();
+            n.pc = pc;
+            return n;
+        }
+        return this;
+    }
+    
+    public Node visitChildren(NodeVisitor v) {
+        abc.aspectj.ast.Pointcut pc = 
+            (abc.aspectj.ast.Pointcut)visitChild(this.pc, v);
+        return reconstruct(pc);
+    }
+
+    public void aspectMethodsEnter(AspectMethods visitor) {
+        //push an empty list of formals. needed for If pointcuts
+        visitor.pushFormals(Collections.unmodifiableList(new LinkedList()));
+    }
+    public Node aspectMethodsLeave(AspectMethods visitor, AJNodeFactory nf,
+            AJTypeSystem ts) {
+        return this;
+    }
+}
