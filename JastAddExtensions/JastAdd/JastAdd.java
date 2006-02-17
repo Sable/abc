868a1e0986eb155fb6da61e804895029d8fdfbe6
torbjorn <torbjorn@40514614-586c-40e6-bf05-bf7e477dc3e6>
2006-02-17 09:32:00 +0000

    *** empty log message ***

--- JastAddExtensions/JastAdd/JastAdd.java
+++ JastAddExtensions/JastAdd/JastAdd.java
@@ -5,8 +5,12 @@ import java.io.*;
 import parser.*;
 
 public class JastAdd {
-
   public static void main(String args[]) {
+    if(!compile(args))
+      System.exit(1);
+  }
+
+  public static boolean compile(String args[]) {
     Program program = new Program();
     program.initOptions();
     program.addKeyValueOption("-classpath");
@@ -15,12 +19,24 @@ public class JastAdd {
     program.addKeyValueOption("-extdirs");
     program.addKeyValueOption("-d");
     program.addKeyOption("-verbose");
+    program.addKeyOption("-version");
+    program.addKeyOption("-help");
+
     program.addKeyOption("-no_cache_cycle");
     program.addKeyOption("-no_visit_check");
     
     program.addOptions(args);
     Collection files = program.files();
     
+    if(program.hasOption("-version")) {
+      printVersion();
+      return false;
+    }
+    if(program.hasOption("-help") || files.isEmpty()) {
+      printUsage();
+      return false;
+    }
+
     JavaParser parser = new JavaParser();
     for(Iterator iter = files.iterator(); iter.hasNext(); ) {
       String name = (String)iter.next();
@@ -35,17 +51,20 @@ public class JastAdd {
         program.addCompilationUnit(unit);
       } catch (Error e) {
         System.err.println(name + ": " + e.getMessage());
-        System.exit(1);
+        return false;
       } catch (RuntimeException e) {
         System.err.println(name + ": " + e.getMessage());
+        return false;
       } catch (IOException e) {
+        System.err.println("error: " + e.getMessage());
+        return false;
       } catch (Exception e) {
         System.err.println(e);
         e.printStackTrace();
       }
     }
     program.updateRemoteAttributeCollections(files.size());
-      if(Program.verbose())
+    if(Program.verbose())
         program.prettyPrint(files.size());
     if(program.errorCheck(files.size())) {
       if(Program.verbose())
@@ -57,6 +76,30 @@ public class JastAdd {
         program.prettyPrint(files.size());
       program.updateRemoteAttributeCollections(files.size());
       program.generateClassfile(files.size());
+      return true;
     }
+    return false;
+  }
+  
+  protected static void printUsage() {
+    printVersion();
+    System.out.println(
+      "\nJastAdd\n\n" +
+      "Usage: java JastAdd <options> <source files>\n" +
+      "  -verbose                  Output messages about what the compiler is doing\n" +
+      "  -classpath <path>         Specify where to find user class files\n" +
+      "  -sourcepath <path>        Specify where to find input source files\n" + 
+      "  -bootclasspath <path>     Override location of bootstrap class files\n" + 
+      "  -extdirs <dirs>           Override location of installed extensions\n" +
+      "  -d <directory>            Specify where to place generated class files\n" +
+      "  -help                     Print a synopsis of standard options\n" +
+      "  -version                  Print version information\n" +
+      "  -no_cache_cycle           Disable cache cycle optimization for circular attributes\n" +
+      "  -no_visit_check           Disable visit checks used to detect circularities for attributes\n"
+    );
+  }
+
+  protected static void printVersion() {
+    System.out.println("JastAdd (Experimental Bootstrapped Version) (http://jastadd.cs.lth.se) Version R20060217");
   }
 }
