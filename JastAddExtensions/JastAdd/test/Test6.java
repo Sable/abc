004a9b3ab0b06a52e9f6711aad0bb935e77e00df
torbjorn <torbjorn@40514614-586c-40e6-bf05-bf7e477dc3e6>
2006-03-09 13:07:52 +0000

    *** empty log message ***

--- JastAddExtensions/JastAdd/test/Test6.java
+++ JastAddExtensions/JastAdd/test/Test6.java
@@ -1,13 +1,13 @@
 package test;
 
 public aspect Test6 {
-  ast Node ::= <Value:int>;
+  ast Node ::= <Value:boolean>;
 
   public static void main(String[] args) {
     System.out.println("AST: single token, explicit primitive type");
-    Node n = new Node(1);
+    Node n = new Node(true);
     System.out.println(n.getValue());
-    n.setValue(0);
+    n.setValue(false);
     System.out.println(n.getValue());
   }
 }
