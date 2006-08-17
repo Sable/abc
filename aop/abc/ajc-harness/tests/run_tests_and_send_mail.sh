0ee1a1a7cc1963d56ca95fee9fd3a109f74246f7
ganesh <ganesh@40514614-586c-40e6-bf05-bf7e477dc3e6>
2006-08-17 20:56:08 +0000

    add support for passing options

--- aop/abc/ajc-harness/tests/run_tests_and_send_mail.sh
+++ aop/abc/ajc-harness/tests/run_tests_and_send_mail.sh
@@ -4,11 +4,15 @@ FILES="full_current.output failed_current.output \
       failed_current.xml passed_current.xml \
       skipped_current.xml"
 
+ADDRESS=$1
+SUBJECT=$2
+shift 2
+
 cd ../../
 CLASSPATH= ant clobber
 CLASSPATH= ant jars
 cd ajc-harness/tests
-nice -n 5 ./runtests abcTests.xml
+nice -n 5 ./runtests $* abcTests.xml
 cvs update $FILES
 # perl -pe's/\&/\&amp;/g' -i passed.xml failed.xml skipped.xml
-./calc_updates.pl "$1" "$2"
+./calc_updates.pl "$ADDRESS" "$SUBJECT"
