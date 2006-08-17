2dae568101ee66bab3a77378d4e6e187a47498c4
ganesh <ganesh@40514614-586c-40e6-bf05-bf7e477dc3e6>
2006-08-17 20:31:02 +0000

    break up update_current_pass_fail_lists

---
+++ aop/abc/ajc-harness/tests/run_tests_and_send_mail.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+
+FILES="full_current.output failed_current.output \
+      failed_current.xml passed_current.xml \
+      skipped_current.xml"
+
+cd ../../
+CLASSPATH= ant clobber
+CLASSPATH= ant jars
+cd ajc-harness/tests
+nice -n 5 ./runtests abcTests.xml
+cvs update $FILES
+# perl -pe's/\&/\&amp;/g' -i passed.xml failed.xml skipped.xml
+./calc_updates.pl | mail -s"test run results" abc@comlab.ox.ac.uk
--- aop/abc/ajc-harness/tests/update_current_pass_fail_lists.sh
+++ aop/abc/ajc-harness/tests/update_current_pass_fail_lists.sh
@@ -4,14 +4,6 @@ FILES="full_current.output failed_current.output \
       failed_current.xml passed_current.xml \
       skipped_current.xml"
 
-cd ../../
-CLASSPATH= ant clobber
-CLASSPATH= ant jars
-cd ajc-harness/tests
-nice -n 5 ./runtests abcTests.xml
-cvs update $FILES
-# perl -pe's/\&/\&amp;/g' -i passed.xml failed.xml skipped.xml
-./calc_updates.pl | mail -s"test run results" abc@comlab.ox.ac.uk
 cp failed.xml failed_current.xml
 cp passed.xml passed_current.xml
 cp skipped.xml skipped_current.xml
