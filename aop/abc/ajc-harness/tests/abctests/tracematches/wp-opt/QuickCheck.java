13172dc61a4c33979602481b092d9ff4ff90dec7
eric <eric@40514614-586c-40e6-bf05-bf7e477dc3e6>
2007-05-30 16:29:07 +0000

    updated test for quick check

--- aop/abc/ajc-harness/tests/abctests/tracematches/wp-opt/QuickCheck.java
+++ aop/abc/ajc-harness/tests/abctests/tracematches/wp-opt/QuickCheck.java
@@ -1,8 +1,18 @@
 public class QuickCheck {
-    public static void f() { System.out.println("f"); }
-    public static void g() { System.out.println("g"); }
+    public static void f() {  }
+    public static void g() {  }
+    
+    public void a() {  }
+    public void b() {  }
+    
     public static void main(String[] args) {
-    	f(); 
+    	//only f(); g() is missing
+    	f();	//<---- a shadow for the first tracematch should
+    	        //      be removed here by the quick-check! 
+    	
+    	//a() and b() both exist but on different objects
+    	new QuickCheck().a();
+    	new QuickCheck().b();
     }
 }
 
@@ -18,13 +28,22 @@ aspect FG {
   	  sym f after : call(* *.f(..));
   	  sym g after : call(* *.g(..));
 
-  	  f g* { }
+  	  f g* { } //can match
     }
 
     tracematch() {
   	  sym f after : call(* *.f(..));
   	  sym g after : call(* *.g(..));
 
-  	  f g | f { }
+  	  f g | f { } //can match
+    }
+}
+
+aspect AB {
+    tracematch(Object o) {
+    	sym a after : call(* *.a(..)) && target(o);
+		sym b after : call(* *.b(..)) && target(o);
+
+    	a b {} //does not match but quick check does not suffice to recognize that
     }
 }
