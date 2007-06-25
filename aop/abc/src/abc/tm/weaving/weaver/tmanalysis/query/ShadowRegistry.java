b05394d872ee64e943c5c2846a3a6f6984416b57
eric <eric@40514614-586c-40e6-bf05-bf7e477dc3e6>
2007-06-25 15:54:22 +0000

    bugfix: conjunct must go to the end!

--- aop/abc/src/abc/tm/weaving/weaver/tmanalysis/query/ShadowRegistry.java
+++ aop/abc/src/abc/tm/weaving/weaver/tmanalysis/query/ShadowRegistry.java
@@ -156,7 +156,7 @@ public class ShadowRegistry {
 	public void conjoinShadowWithResidue(String uniqueShadowId, Residue conjunct) {
 		assert allShadowsToAdviceApplications.containsKey(uniqueShadowId);
 		AdviceApplication aa = (AdviceApplication) allShadowsToAdviceApplications.get(uniqueShadowId);		
-		aa.setResidue(AndResidue.construct(conjunct,aa.getResidue()));
+		aa.setResidue(AndResidue.construct(aa.getResidue(),conjunct));
 
 		//print a warning message (usually for test harness)
 		if(Debug.v().warnWhenAlteringShadow)
