56ed83c802b8641f6d5d6423cf8d9cb234060481
olegm <olegm@40514614-586c-40e6-bf05-bf7e477dc3e6>
2007-11-09 15:32:13 +0000

    Explorer & corrected line offsets in CompilationUnit

--- JastAddExtensions/Eclipse/org.jastadd.plugin.jastadd/src/org/jastadd/plugin/jastadd/Activator.java
+++ JastAddExtensions/Eclipse/org.jastadd.plugin.jastadd/src/org/jastadd/plugin/jastadd/Activator.java
@@ -3,13 +3,17 @@ package org.jastadd.plugin.jastadd;
 import org.jastadd.plugin.jastaddj.JastAddJActivator;
 import org.osgi.framework.BundleContext;
 
-public class Activator extends org.jastadd.plugin.jastaddj.JastAddJActivator {
+public class Activator extends org.jastadd.plugin.BaseJastAddActivator {
 	public static String JASTADD_PLUGIN_ID = "org.jastadd.plugin.jastadd";
 	public static Activator INSTANCE;
 	
 	public Activator() {
 		INSTANCE = this;
-	}	
+	}
+	
+	public static Activator getInstnace() {
+		return INSTANCE;
+	}
 	
 	public String getPluginID() {
 		return JASTADD_PLUGIN_ID;
