d86b7e77baac5a530bbdf610c25734e19894fc41
xiemaisi <xiemaisi@40514614-586c-40e6-bf05-bf7e477dc3e6>
2007-12-04 08:34:22 +0000

    Started on a little test suite.

---
+++ JastAddExtensions/Refactoring/tests/ExtractMethod/test0/out/A.java
@@ -0,0 +1,20 @@
+import java.io.FileNotFoundException;
+
+class A {
+  void m() throws java.io.FileNotFoundException {
+    int i;
+    n();
+    i = 2;
+    for(int j; j < i; ++j) {
+      if(j == 4) throw new java.io.FileNotFoundException("");
+      ++i;
+    }
+    int j = ++i;
+  }
+  A(){
+    super();
+  }
+  private void n() {
+    int i;
+  }
+}
