f64a4678b3d8aa11a77a62b5185809c054ad5bc1
pavel <pavel@40514614-586c-40e6-bf05-bf7e477dc3e6>
2008-01-30 13:39:26 +0000

    tidying

--- aop/abc/aspects/NullResidue.java
+++
@@ -1,8 +0,0 @@
-import polyglot.util.InternalCompilerError;
-import abc.weaving.residues.Residue;
-
-public aspect NullResidue {
-    after () returning(Residue r):execution(* *.*(..)) {
-        throw new InternalCompilerError("null residue returned");
-    }
-}
