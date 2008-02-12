5b8ca0e2322f45f4b27152f1f79b1498779601c6
xiemaisi <xiemaisi@40514614-586c-40e6-bf05-bf7e477dc3e6>
2008-02-12 09:54:04 +0000

    Forgot to add two result files from tests that were failing previously (and shouldn't have).

---
+++ JastAddExtensions/Refactoring/tests/data/Encapsulate/test12/out/A.java
@@ -0,0 +1,20 @@
+package p;
+class A {
+  private int i;
+  A[] a;
+  void m() {
+    a.[preIncI()].preIncI();
+  }
+  A() {
+    super();
+  }
+  public int getI() {
+    return i;
+  }
+  public int setI(int i) {
+    return this.i = i;
+  }
+  public int preIncI() {
+    return ++i;
+  }
+}
