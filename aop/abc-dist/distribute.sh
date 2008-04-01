fadd1e98fe4c496fb92632263578cadaea146695
pavel <pavel@40514614-586c-40e6-bf05-bf7e477dc3e6>
2008-04-01 13:05:17 +0000

    fixing distribute script

--- aop/abc-dist/distribute.sh
+++ aop/abc-dist/distribute.sh
@@ -9,14 +9,17 @@ fi
 rm -rf /srv/www/abc.comlab.ox.ac.uk/html/dists/$1
 cp -a dists/$1 /srv/www/abc.comlab.ox.ac.uk/html/dists/
 
-if [ "$2" = "release" ] 
-   then rm -f /srv/www/abc.comlab.ox.ac.uk/apt/dists/abc/main/binary-all/abc_${1}_all.deb
-        ln -s \
+if [ "$2" = "release" ] ; then
+   credits.pl < CREDITS > /srv/www/abc.comlab.ox.ac.uk/html/people.shtml
+fi
+
+if [ "$3" = "deb" ] ; then
+   rm -f /srv/www/abc.comlab.ox.ac.uk/apt/dists/abc/main/binary-all/abc_${1}_all.deb
+   ln -s \
           /srv/www/abc.comlab.ox.ac.uk/html/dists/$1/files/debian/abc_${1}_all.deb \
           /srv/www/abc.comlab.ox.ac.uk/apt/dists/abc/main/binary-all/
 
-        /srv/www/abc.comlab.ox.ac.uk/scripts/build-apt.sh abc main
-        dist/credits.pl < CREDITS > /srv/www/abc.comlab.ox.ac.uk/html/people.shtml
+   /srv/www/abc.comlab.ox.ac.uk/scripts/build-apt.sh abc main
 fi
 
 echo Done.
