e1b06caab603545061373ba074f1e52a25f20ea7
torbjorn <torbjorn@40514614-586c-40e6-bf05-bf7e477dc3e6>
2008-06-03 20:47:28 +0000

    Various fixes.

--- JastAddExtensions/ConcreteSyntax/test/Transform05.java
+++ JastAddExtensions/ConcreteSyntax/test/Transform05.java
@@ -1,11 +1,12 @@
 package test;
 
 public class Transform05 {
-  {
-    int[][] i = new int[][] { {},{3} };
+
+  int v(String[] args) {
+    v(/*hello*/args);
+    w/*hello*/();
+    return 5;
   }
-  // commas and whitespace surrounding commas belong to the list
-  // open and close parentheses and brackets belong to parent
-  //public static void main(/*hello*/String[] args/**/ , /**/Object o) {
-  //}
+  void w() {
+  }
 }
