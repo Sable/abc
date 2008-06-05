8cc149f8cf41f4e3793607b8890754ffda35abcd
torbjorn <torbjorn@40514614-586c-40e6-bf05-bf7e477dc3e6>
2008-06-05 08:34:18 +0000

    Removed generated files from svn.

--- JastAddExtensions/ControlFlowGraph/AST/CFGNode.java
+++
@@ -1,8 +0,0 @@
-
-package AST;
-import java.util.HashSet;import java.util.LinkedHashSet;import java.io.FileNotFoundException;import java.io.File;import java.util.*;import beaver.*;import java.util.ArrayList;import java.util.zip.*;import java.io.*;import java.util.HashMap;import java.util.Iterator;
-
-public interface CFGNode {
-    // Declared in MyAnalysis.jrag at line 5
- @SuppressWarnings({"unchecked", "cast"})     public int uniqueIndex();
-}
