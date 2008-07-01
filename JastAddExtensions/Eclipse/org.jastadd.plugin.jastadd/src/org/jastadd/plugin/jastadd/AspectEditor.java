0b1d8287df39c2d413cb8f57a879cd83e403464e
emma <emma@40514614-586c-40e6-bf05-bf7e477dc3e6>
2008-07-01 16:44:53 +0000

    aspect editor

---
+++ JastAddExtensions/Eclipse/org.jastadd.plugin.jastadd/src/org/jastadd/plugin/jastadd/AspectEditor.java
@@ -0,0 +1,8 @@
+package org.jastadd.plugin.jastadd;
+
+import org.jastadd.plugin.jastaddj.editor.JastAddJEditor;
+
+public class AspectEditor extends JastAddJEditor {
+	public static final String EDITOR_ID = "org.jastadd.plugin.jastadd.AspectEditor";
+	public static final String EDITOR_CONTEXT_ID = "org.jastadd.plugin.jastadd.AspectEditorContext";
+}
