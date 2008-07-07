3102e1fb46810c55b95ee268416a86f87c31f7de
neil <neil@40514614-586c-40e6-bf05-bf7e477dc3e6>
2008-07-07 19:50:30 +0000

    ModuleCompilationUnits now are parents of its member CompilationUnits, but are code generation is still a bit broken.

--- JastAddExtensions/JastAddModules/src/jastadd/JastAddModules.java
+++ JastAddExtensions/JastAddModules/src/jastadd/JastAddModules.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 import AST.BytecodeParser;
 import AST.CompilationUnit;
 import AST.JavaParser;
+import AST.ModuleCompilationUnit;
 import AST.Options;
 
 //primitive extension archi, change to something polyglot-like if needed
@@ -25,7 +26,6 @@ public class JastAddModules extends JastAdd  {
 						return new parser.JavaParser().parse(is, fileName);
 					}
 				});
-		program.printJAModules();
 		if (!result)
 			return false;
 		jastAdd.generate();
@@ -33,6 +33,16 @@ public class JastAddModules extends JastAdd  {
 	}
 
 	public void generate() {
+		System.out.println("----------Module contents----------");
+		program.printJAModules();
+		System.out.println("----------Module AST before insert----------");
+		program.printJAModuleCUAST(0);
+		program.insertModuleCUs();
+		System.out.println("----------Module AST after insert----------");
+		program.printJAModuleCUAST(0);
+		program.dumpTree();
+		
+		
 		program.generateIntertypeDecls();
 		program.transformation();
 		for (Iterator iter = program.compilationUnitIterator(); iter.hasNext();) {
@@ -40,6 +50,17 @@ public class JastAddModules extends JastAdd  {
 			if (cu.fromSource()) {
 				for (int i = 0; i < cu.getNumTypeDecl(); i++) {
 					cu.getTypeDecl(i).generateClassfile();
+				}
+			}
+			//bad hack, fix by refining compilationUnitIterator() in ClassPath.jrag
+			//does not work completely, innertypes for ASTNode not generated properly
+			if (cu instanceof ModuleCompilationUnit) {
+				for (CompilationUnit childCU : ((ModuleCompilationUnit)cu).getCompilationUnitList()) {
+					if (childCU.fromSource()) {
+						for (int i = 0; i < childCU.getNumTypeDecl(); i++) {
+							childCU.getTypeDecl(i).generateClassfile();
+						}
+					}
 				}
 			}
 		}
