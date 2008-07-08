87ea85b69282a4c7ae9839466444be95b77238a3
emma <emma@40514614-586c-40e6-bf05-bf7e477dc3e6>
2008-07-08 15:25:12 +0000

    Breakpoint fix, to work with Java oriented Debug env

--- JastAddExtensions/Eclipse/org.jastadd.plugin.jastaddj/src/org/jastadd/plugin/jastaddj/editor/debug/JastAddJBreakpoint.java
+++ JastAddExtensions/Eclipse/org.jastadd.plugin.jastaddj/src/org/jastadd/plugin/jastaddj/editor/debug/JastAddJBreakpoint.java
@@ -10,19 +10,20 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.debug.core.DebugException;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.model.IBreakpoint;
-import org.eclipse.debug.core.model.LineBreakpoint;
-import org.jastadd.plugin.jastaddj.JastAddJActivator;
+import org.eclipse.jdt.debug.core.JDIDebugModel;
+import org.eclipse.jdt.internal.debug.core.breakpoints.JavaLineBreakpoint;
 import org.jastadd.plugin.model.JastAddModel.FileInfo;
 
-public class JastAddJBreakpoint extends LineBreakpoint {
+public class JastAddJBreakpoint extends JavaLineBreakpoint {
 
 	public static final String MARKER_ID = "org.jastadd.plugin.jastaddj.BreakpointMarker";
-	//public static final String MARKER_ID = "org.eclipse.jdt.debug.javaLineBreakpointMarker";
+	private static final String JAVA_LINE_BREAKPOINT = "org.eclipse.jdt.debug.javaLineBreakpointMarker";
+	
 	protected FileInfo fileInfo;
 	protected String typeName;
 	protected int lineNumber;
 	
-	public JastAddJBreakpoint(final IResource resource, String typeName, FileInfo fileInfo, 
+	public JastAddJBreakpoint(final IResource resource, final String typeName, FileInfo fileInfo, 
 			final int lineNumber, final Map<String,Object> attributes) throws DebugException {
 		this.fileInfo = fileInfo;
 		this.lineNumber = lineNumber;
@@ -30,8 +31,13 @@ public class JastAddJBreakpoint extends LineBreakpoint {
 		IWorkspaceRunnable wr = new IWorkspaceRunnable() {
 			public void run(IProgressMonitor monitor) throws CoreException {
 	
+				// Java stuff taken from JavaLineBreakpoint
+				attributes.put(TYPE_NAME, typeName);
+				attributes.put(SUSPEND_POLICY, new Integer(getDefaultSuspendPolicy()));
+				
 				// create the marker
-				setMarker(resource.createMarker(MARKER_ID));
+				//setMarker(resource.createMarker(MARKER_ID));
+				setMarker(resource.createMarker(JAVA_LINE_BREAKPOINT));
 				// add attributes
 				attributes.put(IBreakpoint.ID, getModelIdentifier());
 				attributes.put(IBreakpoint.ENABLED, Boolean.valueOf(true));
@@ -56,10 +62,13 @@ public class JastAddJBreakpoint extends LineBreakpoint {
 		}
 	}	
 	
+	/*
 	@Override
 	public String getModelIdentifier() {
-		return JastAddJActivator.JASTADDJ_PLUGIN_ID;
+		return JDIDebugModel.getPluginIdentifier();
+		//return JastAddJActivator.JASTADDJ_PLUGIN_ID;
 	}
+	*/
 
 	public boolean sameAs(String name, FileInfo info, int line) {
 		return typeName.equals(name) &&
