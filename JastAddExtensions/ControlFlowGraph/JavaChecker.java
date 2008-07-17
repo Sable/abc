cc1592aa5c71aa1aaf3fc8626c700250d62b3f3a
tingting <tingting@40514614-586c-40e6-bf05-bf7e477dc3e6>
2008-07-17 08:41:48 +0000

    Problems in LivenessAnalysis & NullnessAnalysis

--- JastAddExtensions/ControlFlowGraph/JavaChecker.java
+++ JastAddExtensions/ControlFlowGraph/JavaChecker.java
@@ -4,8 +4,19 @@ import java.io.*;
 
 class JavaChecker extends Frontend {
 
-  public static void main(String args[]) {
-    compile(args);
+  public static void main(String args[]) {  
+    //compile(args);
+	  //add
+	  try {
+		  Vector<String> files = readfiles(args[0]);
+		  String[] paths = new String[files.size()];
+		  files.toArray(paths);
+		  
+		  compile(paths);
+	  }
+	  catch (FileNotFoundException ex) {}			
+	  catch (IOException ex) {}
+	 	  
   }
 
   public static boolean compile(String args[]) {
@@ -26,4 +37,36 @@ class JavaChecker extends Frontend {
     System.out.println(unit);
     System.out.println(unit.dumpTree());
   }
+  
+  
+	public static Vector<String> readfiles(String filepath) throws FileNotFoundException, IOException {
+		Vector<String> files = new Vector<String>();
+		try {			
+			File file = new File(filepath);
+			if (file.isDirectory()) {
+				String[] filelist = file.list();
+				for (int i = 0; i < filelist.length; i++) {
+					File readfile = new File(filepath + "\\" + filelist[i]);
+					if (!readfile.isDirectory()) 
+						files.addAll(readfiles(readfile.getPath()));         
+					else 
+						files.addAll(readfiles(filepath + "\\" + filelist[i]));
+				}
+			}
+			else { 
+				String suffix = "";
+				if(filepath.contains("."))
+					suffix = filepath.substring(filepath.lastIndexOf("."));					
+                if(suffix.equals(".java")) 
+					files.add(filepath);	
+			}
+		}
+		catch (FileNotFoundException e) {
+	    	System.out.println("readfile() Exception:" + e.getMessage());
+		}
+		
+	    return files;
+	}	
+
+
 }
---
+++ JastAddExtensions/ControlFlowGraph/tmp/NullPointerClass.java
@@ -0,0 +1,22 @@
+package tmp;
+
+
+public class NullPointerClass {
+
+	public static void main(String[] args) {
+		String aaa;
+		int bbb;
+		String foo = null;
+		String bar = new String("Hello");
+		String baz = "World";
+		if(foo != null)
+			System.out.println(foo);
+		if(bar != null)
+			System.out.println(bar);
+		if(baz != null)
+			System.out.println(baz);
+		
+
+	}
+
+}
