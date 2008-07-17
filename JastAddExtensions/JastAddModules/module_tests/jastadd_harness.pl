16094ba34756755851bcf2a8d9a7c250b14723f8
neil <neil@40514614-586c-40e6-bf05-bf7e477dc3e6>
2008-07-17 13:03:58 +0000

    perl version of runtest

--- JastAddExtensions/JastAddModules/module_tests/jastadd_harness.pl
+++ JastAddExtensions/JastAddModules/module_tests/jastadd_harness.pl
@@ -1,8 +1,50 @@
 #!/usr/bin/perl
 my $TEST_MIN = 1;
-my $TEST_MAX = 63;
+my $TEST_MAX = 100;
+my $JASTADD_BASEDIR = "../";
+my $JASTADD_MODULES_BASEDIR = "../";
+my $TESTDIR = $JASTADD_MODULES_BASEDIR . "/test";
 
+#clean class, out files
+system "rm $JASTADD_BASEDIR/test/*.class";
+system "rm $JASTADD_BASEDIR/test/*.out";
+
+#run tests
 for (my $i = $TEST_MIN; $i <= $TEST_MAX; $i++) {
-	
-}
+	my $test_name = "Test$i";
+	my $test_file = "$JASTADD_BASEDIR/test/$test_name.java";
 
+	#check if test exists
+	open JAVAFILE, "$test_file" or next;
+
+	#compile with jastadd
+	open OPTIONS , "< $JASTADD_BASEDIR/test/$test_name.options";
+	my $options = "";
+	while (my $line = <OPTIONS>) {
+		$line =~ /\s*(\S*)\s*/;
+		$options .= " " . $1;
+	}
+
+	my $files = "$test_file ";
+	$files .= "$JASTADD_BASEDIR/test/ASTNode.java ";
+	$files .= "$JASTADD_BASEDIR/test/List.java ";
+	$files .= "$JASTADD_BASEDIR/test/Opt.java ";
+
+
+	my $cmdline = "java -cp \"$JASTADD_MODULES_BASEDIR/classes;.\" jastadd.JastAddModules $options $files";
+	print "$test_name\n";
+	#print "JastAdd command: $cmdline\n";
+	system $cmdline;
+	
+	#run generated file
+	my $result_file = "$TESTDIR/$test_name.out";
+	my $correct_file = "$TESTDIR/$test_name.result";
+	my $javacmdline = "java -cp \"$JASTADD_MODULES_BASEDIR/classes;$JASTADD_MODULES_BASEDIR\" test.$test_name > $result_file";
+	#print "Java command: $javacmdline\n";
+	system $javacmdline;
+
+	#diff results
+	system "dos2unix $result_file";
+	system "dos2unix $correct_file";
+	system "diff $result_file $correct_file";
+}
