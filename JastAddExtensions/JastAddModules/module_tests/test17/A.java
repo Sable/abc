1b2656cef99b2960abca92d045170854e73f9b51
neil <neil@40514614-586c-40e6-bf05-bf7e477dc3e6>
2008-07-19 17:03:36 +0000

    changed test17

--- JastAddExtensions/JastAddModules/module_tests/test17/A.java
+++ JastAddExtensions/JastAddModules/module_tests/test17/A.java
@@ -4,4 +4,9 @@ import java.util.LinkedList;
 
 public class A {
 	List<A> list = new LinkedList<A>();
+	Type1 t1 = new Type1();
+	B b = new B();
+	Type2 t2 = new Type2();
+	m2.m3.C c = new m2.m3.C();
+	m2.m3.Type3 t3 = new m2.m3.Type3();
 }
--- JastAddExtensions/JastAddModules/module_tests/test17/B.java
+++ JastAddExtensions/JastAddModules/module_tests/test17/B.java
@@ -1,2 +1,5 @@
 module m2;
-public class B{}
+public class B{
+	C c = new C();
+	Type3 t3 = new Type3();
+}
