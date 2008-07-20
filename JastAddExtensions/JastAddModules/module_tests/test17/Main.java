a866e11f82b43347136e3ba13510f19eaffa2b36
neil <neil@40514614-586c-40e6-bf05-bf7e477dc3e6>
2008-07-20 00:18:38 +0000

    Fixed duplicate ITD generation

---
+++ JastAddExtensions/JastAddModules/module_tests/test17/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String args[]) {
+		m1.A a = new m1.A();
+		m2.B b = new m2.B();
+		m2.m3.C c = new m2.m3.C();
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+}
