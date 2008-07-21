400f7443dbc1c9579f033d665e733ba190bdf8ac
neil <neil@40514614-586c-40e6-bf05-bf7e477dc3e6>
2008-07-21 16:23:57 +0000

    Made lookupModule unlazy, added test

---
+++ JastAddExtensions/JastAddModules/module_tests/test27/B.java
@@ -0,0 +1,9 @@
+module m2;
+
+public class B{
+	public B() {
+		System.out.println(this.getClass());
+		System.out.print("From m2.B: ");
+		m4.D d = new m4.D();
+	}
+}
---
+++ JastAddExtensions/JastAddModules/module_tests/test27/C.java
@@ -0,0 +1,9 @@
+module m3;
+
+public class C{
+	public C() {
+		System.out.println(this.getClass());
+		System.out.print("From m3.C: ");
+		m4.D d = new m4.D();
+	}
+}
