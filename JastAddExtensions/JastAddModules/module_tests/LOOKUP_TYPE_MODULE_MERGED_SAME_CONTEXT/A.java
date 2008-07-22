047e0a340bf1aa418301f356b82747354ab84af7
neil <neil@40514614-586c-40e6-bf05-bf7e477dc3e6>
2008-07-22 18:36:07 +0000

    Added lookup test for same context merge

---
+++ JastAddExtensions/JastAddModules/module_tests/LOOKUP_TYPE_MODULE_MERGED_SAME_CONTEXT/A.java
@@ -0,0 +1,16 @@
+module m1;
+
+public class A {
+	BType b1 = new BType();
+	m2alias.BType b2 = new m2alias.BType();
+
+	B b3 = new B();
+	m2alias.B b4 = new m2alias.B();
+
+	m2alias.m3alias.CType c1 = new m2alias.m3alias.CType();
+	m2alias.m3alias.C c2 = new m2alias.m3alias.C();
+
+	public A() {
+		System.out.println(this.getClass());
+	}
+}
---
+++ JastAddExtensions/JastAddModules/module_tests/LOOKUP_TYPE_MODULE_MERGED_SAME_CONTEXT/B.java
@@ -0,0 +1,14 @@
+module m2;
+public class B {
+	m3alias.CType c1 = new m3alias.CType();
+	m3c.CType c2 = new m3c.CType();
+	m3d.CType c3 = new m3d.CType();
+
+	m3alias.C c4 = new m3alias.C();
+	m3c.C c5 = new m3c.C();
+	m3d.C c6 = new m3d.C();
+
+	public B() {
+		System.out.println(this.getClass());
+	}
+}
