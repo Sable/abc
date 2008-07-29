43a80e462a21c717d490d68905ad855fb693673a
neil <neil@40514614-586c-40e6-bf05-bf7e477dc3e6>
2008-07-29 17:54:52 +0000

    Java case study

---
+++ JastAddExtensions/JastAddModules/case_study/lib/List.java
@@ -0,0 +1,11 @@
+//module jastadd$framework;
+public class List extends ASTNode implements Cloneable {
+  public List() {
+    super();
+  }
+
+  public List add(ASTNode node) {
+    addChild(node);
+    return this;
+  }
+}
