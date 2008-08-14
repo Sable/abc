01f73d779427de157cdfb9dcb862adb56b9df9f7
neil <neil@40514614-586c-40e6-bf05-bf7e477dc3e6>
2008-08-14 11:04:34 +0000

    Removed debug from error tests for portability

--- JastAddExtensions/JastAddModules/module_tests/ERROR_BASE_MODULE_MERGE2/test.sh
+++ JastAddExtensions/JastAddModules/module_tests/ERROR_BASE_MODULE_MERGE2/test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ../testlib/pretest.sh
-../testlib/ja-modules.sh -jastaddframework  -debug -d classes -instance-module m1 *.module *.java 2>&1 > out
-../testlib/posttest.sh
+../testlib/ja-modules.sh -jastaddframework -d classes -instance-module m1 *.module *.java 2>&1 > out
+../testlib/posttesterr.sh
--- JastAddExtensions/JastAddModules/module_tests/ERROR_INCOMPATIBLE_MERGE/test.sh
+++ JastAddExtensions/JastAddModules/module_tests/ERROR_INCOMPATIBLE_MERGE/test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ../testlib/pretest.sh
-../testlib/ja-modules.sh -jastaddframework  -debug -d classes -instance-module M1 *.java *.module 2>&1 > out
-../testlib/posttest.sh
+../testlib/ja-modules.sh -jastaddframework -d classes -instance-module M1 *.java *.module 2>&1 > out
+../testlib/posttesterr.sh
