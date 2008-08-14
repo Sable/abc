fdcd2d080ded24fcfc9b45e809b83b77328e7e88
neil <neil@40514614-586c-40e6-bf05-bf7e477dc3e6>
2008-08-14 15:51:07 +0000

    Added file args wrapper for debugging long arguments

---
+++ JastAddExtensions/JastAddModules/src/jastadd/JastAddModulesFileArgs.java
@@ -0,0 +1,28 @@
+package jastadd;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
+public class JastAddModulesFileArgs {
+	public static void main(String args[]) {
+		LinkedList<String> fileArgs = new LinkedList<String>();
+		if (args.length != 1) {
+			System.err.println("Usage: JastAddModulesFileArgs <argsfile>");
+		}
+		String infile = args[0];
+		try {
+			BufferedReader r = new BufferedReader(new InputStreamReader(new FileInputStream(infile)));
+			while (r.ready()) {
+				String line = r.readLine().trim();
+				fileArgs.add(line);
+				System.out.println(line);
+			}
+			new JastAddModules().compile(fileArgs.toArray(new String[0]));
+		} catch (Exception e) {
+			System.out.println(e);
+			e.printStackTrace();
+		}
+	}
+}
