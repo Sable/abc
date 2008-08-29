ca24a3b7f5b0826abd319b71858ac12d28478baf
mathieu <mathieu@40514614-586c-40e6-bf05-bf7e477dc3e6>
2008-08-29 17:44:51 +0000

    Removed the dummy memoline at bottom of the stack

--- JastAddExtensions/JastAddIncremental/lib/Main.java
+++ JastAddExtensions/JastAddIncremental/lib/Main.java
@@ -6,18 +6,19 @@ public class Main {
 	public static final boolean DEBUG = false;
 	
 	private static Stack<MemoLine> memostack = new Stack<MemoLine>();
-	private static MemoLine curline;
-	static { memostack.push(curline=new MemoLine()); }
+	private static MemoLine curline = null;
 	
 	public static void pushMemoLine(MemoLine line) {
 		memostack.push(curline=line);
 	}
 	public static void popMemoLine() {
 		memostack.pop();
-		curline = memostack.peek();
+		curline = memostack.isEmpty()? null : memostack.peek();
 	}
 	public static void registerDependency(Dependency dep, Object val) {
-		curline.add(dep, val);
+		if (curline != null) {
+			curline.add(dep, val);
+		}
 	}
 
 }
