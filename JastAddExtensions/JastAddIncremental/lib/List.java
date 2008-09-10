932971e73db95a71096f8379fa3a0d1f5dd8e165
torbjorn <torbjorn@40514614-586c-40e6-bf05-bf7e477dc3e6>
2008-09-10 13:14:06 +0000

    Updated to latest version of JastAdd. Disabled depedency tracking during rewriting.

--- JastAddExtensions/JastAddIncremental/lib/List.java
+++ JastAddExtensions/JastAddIncremental/lib/List.java
@@ -1,11 +1,34 @@
 package AST;
-public class List extends ASTNode implements Cloneable {
+public class List<T extends ASTNode> extends ASTNode<T> implements Cloneable {
   public List() {
     super();
   }
 
-  public List add(ASTNode node) {
+  public List<T> add(T node) {
     addChild(node);
     return this;
   }
+  public void insertChild(T node, int i) {
+    list$touched = true;
+    super.insertChild(node, i);
+  }
+
+  public void addChild(T node) {
+    list$touched = true;
+    super.addChild(node);
+  }
+
+  public void removeChild(int i) {
+    list$touched = true;
+    super.removeChild(i);
+  }
+  public int getNumChild() {
+    if(list$touched) {
+      for(int i = 0; i < getNumChildNoTransform(); i++)
+        getChild(i);
+      list$touched = false;
+    }
+    return getNumChildNoTransform();
+  }
+  private boolean list$touched = true;
 }
