fa2f4fd0fd4f7fa826656ce7bf6c25b46ccbfe73
xiemaisi <xiemaisi@40514614-586c-40e6-bf05-bf7e477dc3e6>
2008-10-15 10:43:27 +0000

    Tentative support for refactoring XML.

--- JastAddExtensions/Refactoring/JavaChecker.java
+++ JastAddExtensions/Refactoring/JavaChecker.java
@@ -1,22 +1,31 @@
-import AST.*;
+import AST.BytecodeParser;
+import AST.CompilationUnit;
+import AST.Frontend;
+import AST.JavaParser;
+import AST.RefactoringException;
+import AST.TypeDecl;
 
 class JavaChecker extends Frontend {
-  public static void main(String args[]) {
-    compile(args);
-  }
 
-  public static boolean compile(String args[]) {
-    return new JavaChecker().process(
-        args,
-        new BytecodeParser(),
-        new JavaParser() {
-          public CompilationUnit parse(java.io.InputStream is, String fileName) throws java.io.IOException, beaver.Parser.Exception {
-            return new parser.JavaParser().parse(is, fileName);
-          }
-        }
-    );
-  }
+	public static void main(String args[]) throws RefactoringException {
+		JavaChecker checker = new JavaChecker();
+		checker.compile(args);
+		System.out.println("Before: "+checker.program);
+		TypeDecl A = checker.program.lookupType("p", "A");
+		A.rename("AA");
+		System.out.println("After: "+checker.program);
+	}
 
-  protected String name() { return "Java1.4Frontend + Java1.5Extensions"; }
-  protected String version() { return "R20071015"; }
+	public boolean compile(String args[]) {
+		return process(
+				args,
+				new BytecodeParser(),
+				new JavaParser() {
+					public CompilationUnit parse(java.io.InputStream is, String fileName) throws java.io.IOException, beaver.Parser.Exception {
+						return new parser.JavaParser().parse(is, fileName);
+					}
+				}
+		);
+	}
+
 }
