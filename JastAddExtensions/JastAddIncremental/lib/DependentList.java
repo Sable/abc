a53a0735995d68e9e695b7b7b281f68b87773651
xiemaisi <xiemaisi@40514614-586c-40e6-bf05-bf7e477dc3e6>
2008-10-27 17:18:30 +0000

    This version yields a compiler that compiles Jigsaw. The treatment of NTAs is very ad hoc and many cases don't work.

--- JastAddExtensions/JastAddIncremental/lib/DependentList.java
+++ JastAddExtensions/JastAddIncremental/lib/DependentList.java
@@ -3,32 +3,63 @@ package AST;
 import java.util.Deque;
 import java.util.LinkedList;
 
+import AST.Caching.CacheRoot;
+
 public class DependentList {
-    private ASTNode home;
-    private Deque<Caching.CacheRoot> deps = new LinkedList<Caching.CacheRoot>();
-
-    public DependentList(ASTNode home) {
-	this.home = home;
+  private AST.ASTNode home;
+  private Object deps;
+  public DependentList(AST.ASTNode home) {
+    super();
+    this.home = home;
+  }
+  public void add(AST.Caching.CacheRoot root) {
+    /* ideally, the root should never be null, since every node
+       should have a cache root
+       sometimes, however, we may call lexeme accessors on nodes that
+       are not in the tree yet, which might not have cache roots */
+    if(root == null)
+      return;
+    if(deps == null) {
+      deps = root;
+    } else if(deps != root) {
+      if(deps instanceof CacheRoot) {
+	Deque<CacheRoot> d = new LinkedList<CacheRoot>();
+	d.add(root);
+	d.add((CacheRoot)deps);
+	deps = d;
+      } else {
+	Deque<CacheRoot> d = (Deque<CacheRoot>)deps;
+	if(!d.contains(root)) 
+	  d.add(root);
+      }
     }
-
-    public void add(Caching.CacheRoot root) {
-      /* ideally, the root should never be null, since every node
-	 should have a cache root
-	 sometimes, however, we may call lexeme accessors on nodes that
-	 are not in the tree yet, which might not have cache roots */
-	if(root != null && !deps.contains(root))
-	    deps.add(root);
-    }
-
-    public void propagate() {
-        add(home.getCacheRoot());
-	int sz = deps.size();
-	while(sz-- > 0) {
-	    deps.poll().propagate();
+  }
+  public void propagate() {
+    CacheRoot root = home.getCacheRoot();
+    if(deps == null || deps == root) {
+      deps = null;
+      root.propagate();
+    } else if(deps instanceof Deque) {
+      Deque<CacheRoot> d = (Deque<CacheRoot>)deps;
+      if(!d.contains(root)) {
+	root.propagate();
+	int sz = d.size();
+	while(--sz > 0)
+	  d.poll().propagate();
+      } else {
+	int sz = d.size();
+	while(sz-- > 0){
+	  d.poll().propagate();
 	}
+      }
+    } else {
+      CacheRoot r = (CacheRoot)deps;
+      deps = null;
+      root.propagate();
+      r.propagate();
     }
-
-    public String toString() {
-	return deps.toString();
-    }
+  }
+  public java.lang.String toString() {
+    return deps.toString();
+  }
 }
