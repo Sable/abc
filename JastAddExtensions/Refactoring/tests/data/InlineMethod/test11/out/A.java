e5b7adde5ad0edba057070e8d8d95c6a2b839bcd
xiemaisi <xiemaisi@40514614-586c-40e6-bf05-bf7e477dc3e6>
2008-11-24 10:20:17 +0000

    Inline Method is now smarter about handling return statements.

--- JastAddExtensions/Refactoring/tests/data/InlineMethod/test11/out/A.java
+++ JastAddExtensions/Refactoring/tests/data/InlineMethod/test11/out/A.java
@@ -1,11 +1,10 @@
 class A {
   void m() {
     int j;
-    l:{
+    {
       int i = 23;
       System.out.println("here");
       j = i = 42;
-      break l;
     }
     System.out.println("back");
   }
--- JastAddExtensions/Refactoring/tests/data/InlineMethod/test12/out/A.java
+++ JastAddExtensions/Refactoring/tests/data/InlineMethod/test12/out/A.java
@@ -2,11 +2,10 @@ class A {
   void m() {
     int j;
     int i = 23;
-    l:{
+    {
       int i0 = i;
       System.out.println("here");
       j = i0 = 42;
-      break l;
     }
     System.out.println("back");
   }
