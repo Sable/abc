5ab683b0a536b156ce32631e574abccb27fda933
neil <neil@40514614-586c-40e6-bf05-bf7e477dc3e6>
2008-12-09 12:51:05 +0000

    fixed comment

--- JastAddExtensions/JastAddModulesOSGITranslator/src/jastaddmodules/translator/BundleEnvironmentTranslator.java
+++ JastAddExtensions/JastAddModulesOSGITranslator/src/jastaddmodules/translator/BundleEnvironmentTranslator.java
@@ -29,22 +29,18 @@ import org.osgi.framework.Version;
 public class BundleEnvironmentTranslator {
 	// Passes:
 	// generateOOModules -> creates an AbstractModule instance for each
-	// BundleInstance (includes exported packages)
+	// 		BundleInstance (includes exported packages)
 	// generateOverrides -> generates the overrides relations from the versions.
-	// implicit assumption that a higher version overrides a lower version
+	// 		implicit assumption that a higher version overrides a lower version
 	// generateRBInterfaces -> creates ModuleInterfaces derived from the
-	// required
-	// bundle constraints, and adds these as imports and
-	// implements in the appropriate modules
+	// 		required bundle constraints, and adds these as imports and
+	// 		implements in the appropriate modules
 	// generateIPInterfaces -> creates WeakModuleInterfaces derived from the
-	// import
-	// package declarations
+	// 		import package declarations
 	// generateSystemModule -> generates the top level system module, which does
-	// the
-	// wiring using replace declarations to fill in
-	// the interface references with an actual module. Should
-	// take into consideration the singleton attribute of
-	// bundles
+	// 		the wiring using replace declarations to fill in the interface references 
+	//		with an actual module. Should take into consideration the singleton 
+	//		attribute of bundles
 
 	StaticBundleEnvironment bundleEnv;
 	HashMap<BundleDescription, AbstractModule> bundleMap = new HashMap<BundleDescription, AbstractModule>();
