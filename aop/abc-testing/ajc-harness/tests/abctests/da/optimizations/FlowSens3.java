8e68c5db626711fddebe4c7bf3df40ea25c153ef
Eric Bodden <Eric Bodden@40514614-586c-40e6-bf05-bf7e477dc3e6>
2009-03-16 15:13:30 +0000

    bugfix for backwards analysis: we need to also add initial configurations of the form (q,TRUE), where q is final, just behind any shadow that could lead to a final state; this is necessary to model matches that could be completed by the current method but then cannot be prolonged; if I am not mistaken then this would not be necessary for tracematches, but it is necessary for ERE in JavaMOP etc.

---
+++ aop/abc-testing/ajc-harness/tests/abctests/da/optimizations/FlowSens3.java
@@ -0,0 +1,36 @@
+public class FlowSens3 {
+	
+	public static void main(String[] args) {
+		f1();
+	}
+	
+	static void f1() {
+		FlowSens3 f = new FlowSens3();
+		f.a(); 	//matches; needs to stay
+		f.b(); 	//matches; needs to stay
+		f.a(); 	//could be removed but may be hard to tell by our analysis
+	}
+	
+	void a(){}
+	void b(){}
+	
+}
+
+aspect Foo {
+	
+	dependent before a(Object o): call(* *.a()) && target(o) {
+		
+	}
+	
+	dependent before b(Object o): call(* *.b()) && target(o) {
+		
+	}
+	
+	dependency {
+		a,b;
+		initial s1: a -> s2;
+				s2: b -> s3;
+		final s3;
+	}
+	
+}
