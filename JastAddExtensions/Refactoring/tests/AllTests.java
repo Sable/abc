dbdd59ff2f982d214a2dd5ebe22344a16561901c
xiemaisi <xiemaisi@40514614-586c-40e6-bf05-bf7e477dc3e6>
2009-08-21 05:47:56 +0000

    Some work on making method name locking more correct.

---
+++ JastAddExtensions/Refactoring/tests/AllTests.java
@@ -0,0 +1,20 @@
+package tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite("Refactoring tests");
+		//$JUnit-BEGIN$
+		suite.addTestSuite(RenameMethodTests.class);
+		suite.addTestSuite(RenameVariableTests.class);
+		suite.addTestSuite(RenameTypeTests.class);
+		suite.addTestSuite(MoveMethodTests.class);
+		suite.addTestSuite(MakeMethodStaticTests.class);
+		//$JUnit-END$
+		return suite;
+	}
+
+}
