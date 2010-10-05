82de8f91ebf2e7f28572f80ba9984b359220c5eb
Eric Bodden <Eric Bodden@40514614-586c-40e6-bf05-bf7e477dc3e6>
2010-10-05 15:06:04 +0000

    tagging extracted methods for matching in the backend

---
+++ aop/abc-ja-exts/src-cjp/abc/ja/cjp/weaving/ExtractedTag.java
@@ -0,0 +1,28 @@
+package abc.ja.cjp.weaving;
+
+import soot.tagkit.AttributeValueException;
+import soot.tagkit.Tag;
+
+public class ExtractedTag implements Tag {
+	
+	private final String cjpTypeName;
+
+	public ExtractedTag(String cjpTypeName) {
+		this.cjpTypeName = cjpTypeName;		
+	}
+	
+	public String getCjpTypeName() {
+		return cjpTypeName;
+	}
+
+	@Override
+	public String getName() {
+		return getClass().getName();
+	}
+
+	@Override
+	public byte[] getValue() throws AttributeValueException {
+		throw new AttributeValueException();
+	}
+
+}
