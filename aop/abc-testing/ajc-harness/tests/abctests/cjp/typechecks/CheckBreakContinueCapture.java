97641f0ee725e23752d0c5d385b4da7e0652a146
Eric Bodden <Eric Bodden@40514614-586c-40e6-bf05-bf7e477dc3e6>
2010-10-08 11:14:25 +0000

    type checks for break and continue

---
+++ aop/abc-testing/ajc-harness/tests/abctests/cjp/typechecks/CheckBreakContinueCapture.java
@@ -0,0 +1,48 @@
+public aspect CheckBreakContinueCapture {
+	
+	void normalBreak() {
+		Label:
+		for(int i=0;i<1;i++) {
+			exhibit JP {
+				while(true) {
+					break; //this is ok
+				}
+			};			
+		}
+	}
+	
+	void outerBreak() {
+		Label:
+		for(int i=0;i<1;i++) {
+			exhibit JP {
+				while(true) {
+					break Label; //this should give an error
+				}
+			};			
+		}
+	}
+
+	void normalContinue() {
+		Label:
+		for(int i=0;i<1;i++) {
+			exhibit JP {
+				while(true) {
+					continue; //this is ok
+				}
+			};			
+		}
+	}
+	
+	void outerContinue() {
+		Label:
+		for(int i=0;i<1;i++) {
+			exhibit JP {
+				while(true) {
+					continue Label; //this should give an error
+				}
+			};			
+		}
+	}
+	
+	joinpoint void JP();
+}
