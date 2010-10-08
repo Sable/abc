26e60d52d70ff42c26f8f8eb8dc11f8c2dd46ce8
Eric Bodden <Eric Bodden@40514614-586c-40e6-bf05-bf7e477dc3e6>
2010-10-08 12:35:24 +0000

    another test case

---
+++ aop/abc-testing/ajc-harness/tests/abctests/cjp/typechecks/CheckFieldAccess.java
@@ -0,0 +1,19 @@
+public aspect CheckFieldAccess {
+	
+	int i;
+	final int j=0;
+	
+	void fieldAccess ()  {
+		exhibit JP { i=1; };	// no error
+	}
+	
+	void fieldAccess ()  {
+		exhibit JP { int i=j; };	//no error
+	}
+	
+	void fieldAccess ()  {
+		exhibit JP { j=1; }; //error: assignment to final field 
+	}
+
+	joinpoint void JP();
+}
