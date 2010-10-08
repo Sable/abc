b35af093bba298aaa020e4e38b5f158480d6ee99
Eric Bodden <Eric Bodden@40514614-586c-40e6-bf05-bf7e477dc3e6>
2010-10-08 13:39:00 +0000

    name check for CJPs

---
+++ aop/abc-testing/ajc-harness/tests/abctests/cjp/typechecks/CheckCJPDeclared.java
@@ -0,0 +1,14 @@
+public aspect CheckCJPDeclared {
+	
+	void foo() {
+		exhibit JP {};		//error: not declared
+		exhibit Object {};	//error: not a joinpoint type
+		exhibit Exception {};	//correct
+		exhibit JP2 {};			//correct
+		exhibit CheckCJPDeclared.JP2 {};	//correct		
+	}
+	
+	joinpoint void JP2();
+
+	joinpoint void Exception();
+}
