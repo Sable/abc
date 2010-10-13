245255751d94f22c5b7a88c17f0da28d6c50999c
xiemaisi <xiemaisi@40514614-586c-40e6-bf05-bf7e477dc3e6>
2010-10-13 09:18:36 +0000

    Another small fix to accessibility constraints (for some reason, JastAddJ considers type variables to be "top-level types"); more informative description of type constraints; and some more real programs (now >1 MSLOC).

--- JastAddExtensions/Refactoring/tests/jigsaw/RealProgramTests.java
+++ JastAddExtensions/Refactoring/tests/jigsaw/RealProgramTests.java
@@ -57,6 +57,78 @@ public class RealProgramTests extends TestCase {
 		return checkProgram(compile("tomcat-6.0.x"));
 	}
 	
+	//  62 KSLOC
+	private static Program compileJGroups() throws Exception {
+		return checkProgram(compile("jgroups"));
+	}
+	
+	//  84 KSLOC
+	private static Program compileLucene() throws Exception {
+		return checkProgram(compile("lucene-3.0.1"));
+	}
+	
+	//  41 KSLOC
+	private static Program compileJMeter() throws Exception {
+		return checkProgram(compile("jmeter"));
+	}
+	
+	//  65 KSLOC
+	private static Program compileServingXML() throws Exception {
+		return checkProgram(compile("servingxml-1.1.2"));
+	}
+	
+	//  12 KSLOC
+	private static Program compileJaxen() throws Exception {
+		return checkProgram(compile("org.jaxen 1.1.1"));
+	}
+	
+	//  22 KSLOC
+	private static Program compileHTMLParser() throws Exception {
+		return checkProgram(compile("org.htmlparser 1.6"));
+	}
+	
+	//   2 KSLOC
+	private static Program compileJester() throws Exception {
+		return checkProgram(compile("Jester1.37b"));
+	}
+	
+	//   4 KSLOC
+	private static Program compileJUnit381() throws Exception {
+		return checkProgram(compile("JUnit 3.8.1"));
+	}
+	
+	//   4 KSLOC
+	private static Program compileJUnit382() throws Exception {
+		return checkProgram(compile("JUnit 3.8.2"));
+	}
+	
+	//   5 KSLOC
+	private static Program compileJUnit45() throws Exception {
+		return checkProgram(compile("junit4.5"));
+	}
+	
+	//   2 KSLOC
+	private static Program compileApacheCodec() throws Exception {
+		return checkProgram(compile("org.apache.commons.codec 1.3"));
+	}
+	
+	//   5 KSLOC
+	private static Program compileApacheIO() throws Exception {
+		return checkProgram(compile("org.apache.commons.io 1.4"));
+	}
+	
+	//  23 KSLOC
+	private static Program compileDraw2D() throws Exception {
+		return checkProgram(compile("org.eclipse.draw2d 3.4.2"));
+	}
+	
+	//  29 KSLOC
+	private static Program compileClojure() throws Exception {
+		return checkProgram(compile("clojure-1.1.0"));
+	}
+	
+	// total: 1009 KSLOC
+	
 	private static Program checkProgram(Program prog) {
 		assertNotNull(prog);
 		for(AccessibilityConstraint ac : prog.accessibilityConstraints())
@@ -156,6 +228,20 @@ public class RealProgramTests extends TestCase {
 		compileHSQLDB();
 		compileXalan();
 		compileTomcat();
+		compileJGroups();
+		compileLucene();
+		compileJMeter();
+		compileServingXML();
+		compileJaxen();
+		compileHTMLParser();
+		compileJester();
+		compileJUnit381();
+		compileJUnit382();
+		compileJUnit45();
+		compileApacheCodec();
+		compileApacheIO();
+		compileDraw2D();
+		compileClojure();
 	}
 	
 	// exhaustively generalise parameter types
@@ -181,5 +267,61 @@ public class RealProgramTests extends TestCase {
 	
 	public void testTomcat() throws Exception {
 		exhaustivelyChangeParameterTypes(compileTomcat());
+	}
+	
+	public void testJGroups() throws Exception {
+		exhaustivelyChangeParameterTypes(compileJGroups());
+	}
+	
+	public void testLucene() throws Exception {
+		exhaustivelyChangeParameterTypes(compileLucene());
+	}
+	
+	public void testJMeter() throws Exception {
+		exhaustivelyChangeParameterTypes(compileJMeter());
+	}
+	
+	public void testServingXML() throws Exception {
+		exhaustivelyChangeParameterTypes(compileServingXML());
+	}
+	
+	public void testJaxen() throws Exception {
+		exhaustivelyChangeParameterTypes(compileJaxen());
+	}
+	
+	public void testHTMLParser() throws Exception {
+		exhaustivelyChangeParameterTypes(compileHTMLParser());
+	}
+	
+	public void testJester() throws Exception {
+		exhaustivelyChangeParameterTypes(compileJester());
+	}
+	
+	public void testJUnit381() throws Exception {
+		exhaustivelyChangeParameterTypes(compileJUnit381());
+	}
+	
+	public void testJUnit382() throws Exception {
+		exhaustivelyChangeParameterTypes(compileJUnit382());
+	}
+	
+	public void testJUnit45() throws Exception {
+		exhaustivelyChangeParameterTypes(compileJUnit45());
+	}
+	
+	public void testApacheCodec() throws Exception {
+		exhaustivelyChangeParameterTypes(compileApacheCodec());
+	}
+	
+	public void testApacheIO() throws Exception {
+		exhaustivelyChangeParameterTypes(compileApacheIO());
+	}
+	
+	public void testDraw2D() throws Exception {
+		exhaustivelyChangeParameterTypes(compileDraw2D());
+	}
+	
+	public void testClojure() throws Exception {
+		exhaustivelyChangeParameterTypes(compileClojure());
 	}
 }
