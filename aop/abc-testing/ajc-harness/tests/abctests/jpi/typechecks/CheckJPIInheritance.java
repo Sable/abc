716b8bfe530bcd9b40cfd58da067e4da429c8100
minostro <minostro@40514614-586c-40e6-bf05-bf7e477dc3e6>
2011-07-19 10:56:25 +0000

    added new test cases.

--- aop/abc-testing/ajc-harness/tests/abctests/jpi/typechecks/CheckJPIInheritance.java
+++ aop/abc-testing/ajc-harness/tests/abctests/jpi/typechecks/CheckJPIInheritance.java
@@ -4,14 +4,26 @@
  * Basically, inheritance add arguments to the signature of the jpi.
  */
 
-jpi int JP(char amount); //ok
-jpi int JP2(char items, int x) extends JP(); //ok
-jpi int JP3(char items) extends J(); //wrong, J doesn't exist.
+import java.lang.*;
+import java.io.*;
 
-jpi int JP4(char items) extends JP(item);
-jpi int JP5(char items) extends JP(items);
+jpi Number JP(Number amount); //ok
+jpi Number JP2(char items, int x) extends JP(); //ok
+jpi Number JP3(char items) extends J(); //wrong, J doesn't exist.
 
-jpi char JP6(int z, char b) extends JP(b); //ok
+jpi Number JP4(Number items) extends JP(item); //wrong, item doesn't exist.
+jpi Number JP5(Number items) extends JP(items); //ok
+jpi Number JP6(int z, Number b) extends JP(b); //ok
 
-//jpi Integer JP7(Number amount) extends JP(amount); //wrong, return type
-//jpi Number JP8(Integer amount) extends JP(amount); //wrong, argument type
+jpi Integer JP7(Number amount) extends JP(amount); //wrong, return type
+jpi Number JP8(Integer amount) extends JP(amount); //wrong, argument type
+
+jpi Number JPRoot(Integer x) throws Exception; //ok
+jpi Number JP9(Number m, String z, Integer l) throws Exception extends JPRoot(l); //ok
+jpi Number JP10(Integer x) throws IOException, Exception extends JPRoot(x); //ok
+jpi Number JP11(Integer x) throws IOException extends JPRoot(x); //wrong
+
+jpi Number JPRoot2(); //ok
+jpi Number JP12(Integer z) extends JPRoot2(z); //wrong 
+jpi Number JP13() throws IOException extends JPRoot2(); //ok
+
