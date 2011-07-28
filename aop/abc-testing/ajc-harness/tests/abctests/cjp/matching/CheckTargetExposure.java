3ae97ee4a8d6858113be73125d7ac60df1b74b9a
ebodde <ebodde@40514614-586c-40e6-bf05-bf7e477dc3e6>
2011-07-28 11:50:49 +0000

    fixed test case for thisJoinPoint.getTarget()

--- aop/abc-testing/ajc-harness/tests/abctests/cjp/matching/CheckTargetExposure.java
+++ aop/abc-testing/ajc-harness/tests/abctests/cjp/matching/CheckTargetExposure.java
@@ -2,12 +2,20 @@ import org.aspectj.testing.Tester;
 
 public class CheckTargetExposure {
 	
+	static CheckTargetExposure f;
+	
 	void instanceMethod() {
 		exhibit A.JP { }; 
 	}
 
+	static void staticMethod() {
+		exhibit A.JP2 { }; 
+	}
+	
 	public static void main(String[] args) {
-		new CheckTargetExposure().instanceMethod();
+		f = new CheckTargetExposure();
+		f.instanceMethod();
+		staticMethod();
 	}
 	
 }
@@ -16,7 +24,13 @@ aspect A {
 	
 	joinpoint void JP();
 	
+	joinpoint void JP2();
+
 	void around JP() {
+		Tester.check(thisJoinPoint.getTarget()==CheckTargetExposure.f && thisJoinPoint.getTarget()!=null,"getTarget() returned null or wrong object");
+	}
+	
+	void around JP2() {
 		Tester.check(thisJoinPoint.getTarget()==null,"getTarget() failed to return null, returned instead: "+thisJoinPoint.getTarget());
 	}
 	
