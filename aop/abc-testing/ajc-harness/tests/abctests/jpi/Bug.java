8ac5140c644cd1bd2830d08333c02026925a3dd3
minostro <minostro@40514614-586c-40e6-bf05-bf7e477dc3e6>
2011-08-30 21:05:10 +0000

    new test case.

---
+++ aop/abc-testing/ajc-harness/tests/abctests/jpi/Bug.java
@@ -0,0 +1,31 @@
+import org.aspectj.testing.Tester;
+
+
+jpi int JP(String a);
+jpi int JP2(String a) extends JP(a);
+
+
+public class Bug{
+
+	exhibits int JP(String a): args(a);
+    exhibits int JP2(String a): args(a);
+    
+    int bar(String x){
+    	//Tester.checkEqual(8,x,"we expected 8 and saw "+x);    	
+        return 1;
+    }
+    public static void main(String[] args){
+        new Bug().bar("");
+    }
+
+}
+
+aspect A{
+
+    final int around JP(String a){ 
+    	return proceed(a);
+    }
+    int around JP2(String a){ 
+    	return proceed(a);
+    }
+}
