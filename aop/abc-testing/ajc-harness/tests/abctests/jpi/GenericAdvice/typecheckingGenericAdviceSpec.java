a40180aed659cfbe863d5a44e196966fcdd1dd35
minostro <minostro@40514614-586c-40e6-bf05-bf7e477dc3e6>
2012-02-14 05:20:05 +0000

    GenericAdviceSpec test case.

---
+++ aop/abc-testing/ajc-harness/tests/abctests/jpi/GenericAdvice/typecheckingGenericAdviceSpec.java
@@ -0,0 +1,40 @@
+import java.lang.*;
+
+<R> jpi int JP();
+<R extends Integer> jpi R JP1(R a);
+
+aspect A{
+
+	<L extends Integer> L around JP1(L b){
+		L r = proceed(b);
+		System.out.println("result "+r.toString());
+		return r;
+	}
+	
+	<L extends Integer> before JP1(L i){
+		System.out.println("before "+i.toString());
+	}
+
+	<L extends Integer> after JP1(L i) returning(L o){
+		System.out.println("after returning, return value :"+o.toString());		
+	}		
+	
+	<L extends Integer> after JP1(L i){
+		System.out.println("after "+i.toString());		
+	}
+	
+}
+
+
+public class C{
+	
+	<M extends Integer> exhibits M JP1(M k) : call(M *(..)) && argsinv(k);
+	
+	public static Integer foo(Integer a){return 1;}
+	
+	public static void main(String[] args){
+		foo(new Integer(3));
+	}
+	
+	
+}
