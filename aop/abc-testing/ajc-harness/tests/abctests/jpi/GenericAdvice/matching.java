4060952e24899054afa2c2c1365f17be05be4211
minostro <minostro@40514614-586c-40e6-bf05-bf7e477dc3e6>
2012-02-14 03:32:44 +0000

    update matching test case.

--- aop/abc-testing/ajc-harness/tests/abctests/jpi/GenericAdvice/matching.java
+++ aop/abc-testing/ajc-harness/tests/abctests/jpi/GenericAdvice/matching.java
@@ -2,43 +2,48 @@ import java.lang.*;
 import org.aspectj.testing.Tester;
 
 <R> jpi R JP();
-<R, T>jpi R JP2(T a);
+<R extends Integer>jpi R JP(R a);
+<R extends Number> jpi R JP2();
 
 aspect A{
 	<R> R around JP(){
-		C.counterR++;
+		C.counterJP++;
 		return proceed();
 	}
 	
-	<R, T> R around JP2(T y){
-		C.counterRT++;
-		System.out.println(thisJoinPoint.getSourceLocation());
+	<I extends Integer> I around JP(I y){
+		C.counterJPI++;
 		return proceed(y);
+	}
+
+	<I extends Number> I around JP2(){
+		C.counterJP2++;
+		return proceed();
 	}
 }
 
 public class C{
 	
-	public static int counterR = 0;
-	public static int counterRT = 0;
-	
-	<R, T> exhibits R JP2(T d) : call(* *(..)) && target(d);
-	
-	<R> exhibits R JP() : call(* *(..));
+	public static int counterJP = 0;
+	public static int counterJPI = 0;
+	public static int counterJP2 = 0;
+
+	<R> exhibits R JP() : execution(* *(..));
+	<R extends Integer> exhibits R JP(R d) : execution(R *(..)) && argsinv(d);
+	<R extends Number> exhibits R JP2() : execution(R *(..));
 		
-	public Integer foo2(){
-		return null;
-	}
-	
-	public Float bar2(){
-		return null;
-	}
+	public static Integer foo(Integer a){return null;}
+	public static Float bar(Integer b){return null;}
+	public static Integer foo2(Integer l){return null;}
+	public static Float bar2(Integer a){return null;}
 	
 	public static void main(String[] args){
-		C e = new C();
-		e.foo2();
-		e.bar2();
-		Tester.checkEqual(counterR,2,"expected 2 matches but saw "+counterR);
-		Tester.checkEqual(counterRT,5,"expected 5 matches but saw "+counterRT);
+		foo(new Integer(1));
+		bar(new Integer(2));
+		foo2(new Integer(3));
+		bar2(new Integer(4));
+		Tester.checkEqual(counterJP,5,"expected 5 matches but saw "+counterJP); //4 + 1(match on main(String[]) execution)
+		Tester.checkEqual(counterJPI,2,"expected 2 matches but saw "+counterJPI);
+		Tester.checkEqual(counterJP2,4,"expected 4 matches but saw "+counterJP2);		
 	}	
 }
